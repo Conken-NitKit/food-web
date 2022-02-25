76f0b289a135851459afb01b112c26cff39b9202
平田海輝 <seaglory2004@gmail.com>
2022-02-26 07:52:26 +0900

    add: JITを適用しました。

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 //https://fwywd.com/tech/next-tailwind より
 
 module.exports = {
+  mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   content: [],
   theme: {
