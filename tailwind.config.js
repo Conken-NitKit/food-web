a216a6d76b798594de0c4375e144d3f4ce183e76
Umiteru2004 <umiteru2004@gmail.com>
2022-03-04 09:15:49 +0900

    remove: モードのJIT指定を削除しました。

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 //https://fwywd.com/tech/next-tailwind より
 
 module.exports = {
-  mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   content: [],
   theme: {
