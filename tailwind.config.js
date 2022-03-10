e9bb929ddf612a9c106c05479171487293156b12
平田海輝 <seaglory2004@gmail.com>
2022-03-11 07:27:58 +0900

    add: tailwindに22.5%のカスタム間隔スケールを追加しました。

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 //https://fwywd.com/tech/next-tailwind より
 
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        "22.5/100": "22.5%",
+      },
+    },
   },
   plugins: [],
-}
+};
