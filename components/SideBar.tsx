e2b99d575e05b2212f75422f485563b1aea96e0c
Umiteru2004 <umiteru2004@gmail.com>
2022-03-11 08:16:45 +0900

    add: タイトルを追加しました。

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,5 +1,7 @@
 export const SideBar = () => {
   return (
-    <div className="ml-5/100 h-full w-23/100 bg-gradient-to-t from-co-sb-b to-co-sb-t"></div>
+    <div className="ml-5p pl-2p h-full w-23p bg-gradient-to-t from-co-sb-b to-co-sb-t">
+      <p>コンピュータ研究部</p>
+    </div>
   );
 };
