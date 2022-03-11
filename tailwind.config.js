5290a352032411c489027495e89c196169ede361
Umiteru2004 <umiteru2004@gmail.com>
2022-03-11 09:31:10 +0900

    update: タイトルをスタイリングしました。

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,14 +9,22 @@ module.exports = {
     extend: {
       spacing: {
         "2p": "2%",
+        "3p": "3%",
         "5p": "5%",
-        "13p": "13%",
         "23p": "23%",
       },
 
       colors: {
-        "co-sb-b": "#F8714B", //COlor-SideBar-Bottom
-        "co-sb-t": "#F2A649", //...-Top
+        "co-m-f": "#F8714B", //COlor-SideBar-From
+        "co-m-t": "#F2A649", //...-To
+      },
+
+      fontSize: {
+        "1.8w": "1.8vw",
+      },
+
+      lineHeight: {
+        1.172: "1.172",
       },
     },
   },
