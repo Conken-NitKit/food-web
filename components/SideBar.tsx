5fe7058c24ca752f9d459cbaa519a73a47b30f16
Umiteru2004 <umiteru2004@gmail.com>
2022-03-11 10:48:35 +0900

    update: メニューをリンクにしました。

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export const SideBar = () => {
   return (
     <div className="ml-5p pt-3p pl-2p h-full w-23p bg-gradient-to-t from-co-m-f to-co-m-t">
@@ -5,14 +7,41 @@ export const SideBar = () => {
         コンピュータ研究部
       </p>
       <ul className="mt-13p">
-        <li key="home">ホーム</li>
-        <li key="menu">メニュー</li>
-        <li key="order">注文状況</li>
-        <li key="sales">売上解析</li>
-        <li key="shift">シフト</li>
-        <li key="user">ユーザー管理</li>
-        <li key="audit">監査ログ</li>
-        <li key="settings">設定</li>
+        <li key="home">
+          <Link href="/home">
+            <p>ホーム</p>
+          </Link>
+        </li>
+        <li key="menu">
+          <Link href="/menu">
+            <p>メニュー</p>
+          </Link>
+        </li>
+        <li key="order">
+          <Link href="/order">
+            <p>注文状況</p>
+          </Link>
+        </li>
+        <li key="shift">
+          <Link href="/shift">
+            <p>シフト</p>
+          </Link>
+        </li>
+        <li key="user">
+          <Link href="/user">
+            <p>ユーザー管理</p>
+          </Link>
+        </li>
+        <li key="audit">
+          <Link href="/audit">
+            <p>監査ログ</p>
+          </Link>
+        </li>
+        <li key="settings">
+          <Link href="/settings">
+            <p>設定</p>
+          </Link>
+        </li>
       </ul>
     </div>
   );
