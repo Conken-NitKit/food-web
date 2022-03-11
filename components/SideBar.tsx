dba1065913c3bea7170fd55a9c4b93cb855ee763
平田海輝 <seaglory2004@gmail.com>
2022-03-11 09:31:10 +0900

    update: タイトルをスタイリングしました。

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,7 +1,9 @@
 export const SideBar = () => {
   return (
-    <div className="ml-5p pl-2p h-full w-23p bg-gradient-to-t from-co-sb-b to-co-sb-t">
-      <p>コンピュータ研究部</p>
+    <div className="ml-5p pt-3p pl-2p h-full w-23p bg-gradient-to-t from-co-m-f to-co-m-t">
+      <p className="font-bold text-1.8w text-white leading-1.172">
+        コンピュータ研究部
+      </p>
     </div>
   );
 };
