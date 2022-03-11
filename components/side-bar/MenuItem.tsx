41d5fe215d5913996225b067d706df67598d266e
平田海輝 <seaglory2004@gmail.com>
2022-03-11 13:33:12 +0900

    update: メニューアイテムコンポーネントを作成しました。

--- components/side-bar/MenuItem.tsx
+++ components/side-bar/MenuItem.tsx
@@ -1,1 +1,20 @@
-export const MenuItem = () => {};
+import Link from "next/link";
+
+interface Props {
+  href: string;
+  img: JSX.IntrinsicElements;
+  name: string;
+}
+
+export const MenuItem = ({ href, img, name }: Props) => {
+  return (
+    <li key={href}>
+      <Link href={href}>
+        <p>
+          {img}
+          {name}
+        </p>
+      </Link>
+    </li>
+  );
+};
