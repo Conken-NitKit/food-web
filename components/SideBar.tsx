402ba4997b746844e6711220c5a7328102ef208f
Umiteru2004 <umiteru2004@gmail.com>
2022-03-11 09:40:23 +0900

    add: メニューを追加しました。

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -4,6 +4,16 @@ export const SideBar = () => {
       <p className="font-bold text-1.8w text-white leading-1.172">
         コンピュータ研究部
       </p>
+      <ul>
+        <li key="home">ホーム</li>
+        <li key="menu">メニュー</li>
+        <li key="order">注文状況</li>
+        <li key="sales">売上解析</li>
+        <li key="shift">シフト</li>
+        <li key="user">ユーザー管理</li>
+        <li key="audit">監査ログ</li>
+        <li key="settings">設定</li>
+      </ul>
     </div>
   );
 };
