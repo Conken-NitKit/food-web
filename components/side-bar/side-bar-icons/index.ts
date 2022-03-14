d58bdeda37a59d4816cc46261e8b32d0b1c77220
平田海輝 <seaglory2004@gmail.com>
2022-03-14 17:10:29 +0900

    add: side-bar-icons/index.ts

---
+++ components/side-bar/side-bar-icons/index.ts
@@ -0,0 +1,8 @@
+export { HomeIcon } from "./HomeIcon";
+export { MenuIcon } from "./MenuIcon";
+export { OrderIcon } from "./OrderIcon";
+export { SalesIcon } from "./SalesIcon";
+export { ShiftIcon } from "./ShiftIcon";
+export { UserIcon } from "./UserIcon";
+export { AuditIcon } from "./AuditIcon";
+export { SettingsIcon } from "./SettingsIcon";
