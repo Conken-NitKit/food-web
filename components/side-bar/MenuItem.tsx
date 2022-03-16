970d9341a9f55e21ebf764241c6d05ccac151b68
Umiteru2004 <umiteru2004@gmail.com>
2022-03-16 12:59:10 +0900

    remove: MenuItemからIconsのimportを削除しました。

--- components/side-bar/MenuItem.tsx
+++ components/side-bar/MenuItem.tsx
@@ -2,16 +2,6 @@ import { ReactNode, useContext } from "react";
 import Link from "next/link";
 
 import { ChosenItemContext } from "../../contexts";
-import {
-  AuditIcon,
-  HomeIcon,
-  MenuIcon,
-  OrderIcon,
-  SalesIcon,
-  SettingsIcon,
-  ShiftIcon,
-  UserIcon,
-} from "./menu-item-icons";
 
 interface Props {
   href: string;
@@ -22,33 +12,6 @@ interface Props {
 export const MenuItem = ({ href, name }: Props) => {
   const { chosenItem, setChosenItem } = useContext(ChosenItemContext);
 
-  const MENU = "menu";
-  const ORDER = "order";
-  const SALES = "Sales";
-  const SHIFT = "shift";
-  const USER = "user";
-  const AUDIT = "audit";
-  const SETTINGS = "settings";
-
-  const icon = //hrefに対応したIconの選定
-    href === MENU ? (
-      <MenuIcon chosenItem={chosenItem} />
-    ) : href === ORDER ? (
-      <OrderIcon chosenItem={chosenItem} />
-    ) : href === SALES ? (
-      <SalesIcon chosenItem={chosenItem} />
-    ) : href === SHIFT ? (
-      <ShiftIcon chosenItem={chosenItem} />
-    ) : href === USER ? (
-      <UserIcon chosenItem={chosenItem} />
-    ) : href === AUDIT ? (
-      <AuditIcon chosenItem={chosenItem} />
-    ) : href === SETTINGS ? (
-      <SettingsIcon chosenItem={chosenItem} />
-    ) : (
-      <HomeIcon chosenItem={chosenItem} /> //対応するIconがなければHomeIconを表示
-    );
-
   return (
     <li
       key={href}
@@ -57,10 +20,7 @@ export const MenuItem = ({ href, name }: Props) => {
       }}
     >
       <Link href={href}>
-        <p>
-          {icon}
-          {name}
-        </p>
+        <p>{name}</p>
       </Link>
     </li>
   );
