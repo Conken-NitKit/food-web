e20838060fc5e4a4b0ffb37bef816d2fab186283
Umiteru2004 <umiteru2004@gmail.com>
2022-03-16 23:26:34 +0900

    remove: contexts

--- contexts/ChosenContext.tsx
+++
@@ -1,7 +0,0 @@
-import { Dispatch, SetStateAction } from "react";
-import { createContext } from "react";
-
-export const ChosenContext = createContext<{
-  chosen: string;
-  setChosen: Dispatch<SetStateAction<string>>;
-}>({ chosen: "", setChosen: () => {} });
