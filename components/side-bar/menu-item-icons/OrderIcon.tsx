e4b9b1be29bbfb69107f5e8303c0032b79a9a26b
Umiteru2004 <umiteru2004@gmail.com>
2022-03-19 21:55:12 +0900

    update: OrderIconに選択時に色を変更する処理を追加しました。

--- components/side-bar/menu-item-icons/OrderIcon.tsx
+++ components/side-bar/menu-item-icons/OrderIcon.tsx
@@ -19,13 +19,13 @@ export const OrderIcon = ({ item, chosenItem }: menuItemIconProps) => {
       />
       <path
         d="M33.7383 17V24C33.7383 25.3261 33.1584 26.5979 32.1264 27.5355C31.0943 28.4732 29.6945 29 28.2349 29V29C26.7753 29 25.3755 28.4732 24.3434 27.5355C23.3114 26.5979 22.7316 25.3261 22.7316 24V21H4.02015C3.87525 20.9988 3.73154 21.0239 3.59742 21.0737C3.4633 21.1236 3.34146 21.1972 3.23899 21.2903C3.13653 21.3834 3.0555 21.4941 3.00064 21.6159C2.94578 21.7378 2.91819 21.8684 2.91948 22C2.91948 26 3.38313 29 8.42283 29H28.2349"
-        stroke="white"
+        stroke={isChose ? "url(#paint0_linear_12_300)" : "white"}
         strokeWidth="2"
         strokeLinejoin="round"
       />
       <path
         d="M20.5302 14H29.3356M16.1276 9H29.3356H16.1276Z"
-        stroke="white"
+        stroke={isChose ? "url(#paint1_linear_12_300)" : "white"}
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
