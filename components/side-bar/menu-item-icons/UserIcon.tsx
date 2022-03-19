03c88331ced8afba8dc791b1d21acc92f0dd4e3b
平田海輝 <seaglory2004@gmail.com>
2022-03-20 05:58:11 +0900

    fix: クリック時にUserIconが消える不具合を修正しました。

--- components/side-bar/menu-item-icons/UserIcon.tsx
+++ components/side-bar/menu-item-icons/UserIcon.tsx
@@ -12,26 +12,25 @@ export const UserIcon = ({ item, chosenItem }: menuItemIconProps) => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        d="M26.5839 9C26.3143 12.3044 23.5571 15 20.5302 15C17.5034 15 14.7414 12.305 14.4766 9C14.2014 5.5625 16.8843 3 20.5302 3C24.1762 3 26.8591 5.625 26.5839 9Z"
-        stroke={isChose ? "url(paint0_linear_28_920)" : "white"}
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        d="M26.5838 9C26.3142 12.3044 23.557 15 20.5301 15C17.5033 15 14.7413 12.305 14.4764 9C14.2013 5.5625 16.8842 3 20.5301 3C24.1761 3 26.859 5.625 26.5838 9Z"
+        stroke={isChose ? "url(#paint0_linear_28_920)" : "white"}
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
       />
       <path
-        d="M20.5302 19C14.5453 19 8.47099 22 7.34693 27.6625C7.20935 28.345 7.63655 29 8.42353 29H32.6376C33.4246 29 33.8497 28.345 33.7142 27.6625C32.5894 22 26.5151 19 20.5302 19Z"
-        stroke={isChose ? "url(paint1_linear_28_920)" : "white"}
-        strokeWidth="2"
-        strokeMiterlimit="10"
+        d="M20.5301 19C14.5452 19 8.47087 22 7.34681 27.6625C7.20923 28.345 7.63642 29 8.4234 29H32.6375C33.4245 29 33.8496 28.345 33.7141 27.6625C32.5893 22 26.515 19 20.5301 19Z"
+        stroke={isChose ? "url(#paint1_linear_28_920)" : "white"}
+        stroke-width="2"
+        stroke-miterlimit="10"
       />
       <path
-        d="M10.6241 14.5H2.91943M6.77178 11V18V11Z"
-        stroke={isChose ? "url(paint2_linear_28_920)" : "white"}
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        d="M10.6236 14.5H2.91895M6.77129 11V18V11Z"
+        stroke={isChose ? "url(#paint2_linear_28_920)" : "white"}
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
       />
-
       <defs>
         <linearGradient
           id="paint0_linear_28_920"
