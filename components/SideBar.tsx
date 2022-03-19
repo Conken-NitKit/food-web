12a6a16e3397e938df8eb23ae51e8ffe7b4506e0
平田海輝 <seaglory2004@gmail.com>
2022-03-20 08:52:09 +0900

    update: SideBarのfontSizeとborderRadiusの指定を[数]w→[数]に変更しました。

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -60,7 +60,7 @@ export const SideBar = () => {
 
   return (
     <div className="ml-5p pt-3p pl-2p w-23p h-full bg-gradient-to-t from-co-m-f to-co-m-t">
-      <p className="mb-13p font-bold text-1.8w text-white leading-1.172">
+      <p className="mb-13p font-bold text-1.8 text-white leading-1.172">
         コンピュータ研究部
       </p>
       <ul>
@@ -71,7 +71,7 @@ export const SideBar = () => {
               setChosen(menuItemProps.href);
             }}
             className={`pl-9p h-4w${
-              menuItemProps.href === chosen ? " rounded-l-1w bg-white" : ""
+              menuItemProps.href === chosen ? " rounded-l-1 bg-white" : ""
             }`}
           >
             <Link href={menuItemProps.href}>
