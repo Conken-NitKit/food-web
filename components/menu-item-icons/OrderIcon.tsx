6be12bd138dda1ec73b940071237eb891cd120c1
平田海輝 <seaglory2004@gmail.com>
2022-03-20 06:22:10 +0900

    update: OrderIconの一部プロパティをケバブ→キャメルケースに変更しました。

--- components/menu-item-icons/OrderIcon.tsx
+++ components/menu-item-icons/OrderIcon.tsx
@@ -14,21 +14,21 @@ export const OrderIcon = ({ item, chosenItem }: menuItemIconProps) => {
       <path
         d="M11.7251 21V3L13.9264 4L16.1278 3L18.325 4L20.5518 3L22.7318 4L24.9187 3L27.1152 4L29.3358 3L31.5379 4L33.7385 3V17"
         stroke={isChose ? "url(#paint0_linear_12_300)" : "white"}
-        stroke-width="2"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinejoin="round"
       />
       <path
         d="M33.7383 17V24C33.7383 25.3261 33.1584 26.5979 32.1264 27.5355C31.0943 28.4732 29.6945 29 28.2349 29V29C26.7753 29 25.3755 28.4732 24.3434 27.5355C23.3114 26.5979 22.7316 25.3261 22.7316 24V21H4.02015C3.87525 20.9988 3.73154 21.0239 3.59742 21.0737C3.4633 21.1236 3.34146 21.1972 3.23899 21.2903C3.13653 21.3834 3.0555 21.4941 3.00064 21.6159C2.94578 21.7378 2.91819 21.8684 2.91948 22C2.91948 26 3.38313 29 8.42283 29H28.2349"
         stroke={isChose ? "url(#paint1_linear_12_300)" : "white"}
-        stroke-width="2"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinejoin="round"
       />
       <path
         d="M20.5301 14H29.3355M16.1274 9H29.3355H16.1274Z"
         stroke={isChose ? "url(#paint2_linear_12_300)" : "white"}
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <defs>
         <linearGradient
@@ -39,8 +39,8 @@ export const OrderIcon = ({ item, chosenItem }: menuItemIconProps) => {
           y2="21"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#F2A649" />
-          <stop offset="1" stop-color="#F8714B" />
+          <stop stopColor="#F2A649" />
+          <stop offset="1" stopColor="#F8714B" />
         </linearGradient>
         <linearGradient
           id="paint1_linear_12_300"
@@ -50,8 +50,8 @@ export const OrderIcon = ({ item, chosenItem }: menuItemIconProps) => {
           y2="29"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#F2A649" />
-          <stop offset="1" stop-color="#F8714B" />
+          <stop stopColor="#F2A649" />
+          <stop offset="1" stopColor="#F8714B" />
         </linearGradient>
         <linearGradient
           id="paint2_linear_12_300"
@@ -61,8 +61,8 @@ export const OrderIcon = ({ item, chosenItem }: menuItemIconProps) => {
           y2="14"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#F2A649" />
-          <stop offset="1" stop-color="#F8714B" />
+          <stop stopColor="#F2A649" />
+          <stop offset="1" stopColor="#F8714B" />
         </linearGradient>
       </defs>
     </svg>
