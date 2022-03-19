bbf4f67eb208c950cf15cc729d0a91644b414c36
平田海輝 <seaglory2004@gmail.com>
2022-03-20 06:06:23 +0900

    move: side-bar/index, SideBar.tsx, menu-item-iconsをside-bar→componentsに移動しました

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 
-import { SideBar } from "../components/side-bar";
+import { SideBar } from "../components";
 
 const Home: NextPage = () => {
   return (
