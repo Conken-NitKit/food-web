c5d1011f0a1b679394e52c476b70acc187dabe37
Umiteru2004 <umiteru2004@gmail.com>
2022-03-20 11:49:33 +0900

    rename: tailwindのcolorsのco-m-f→m-fに改名しました。

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,7 @@ module.exports = {
       },
 
       colors: {
-        "co-m-f": "#F8714B", //COlor-SideBar-From
+        "m-f": "#F8714B", //COlor-SideBar-From
         "co-m-t": "#F2A649", //...-To
         m: "#F58B4A", //Main
       },
