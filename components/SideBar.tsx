fa82098ad20436553c1028c5e76441e007676374
Umiteru2004 <umiteru2004@gmail.com>
2022-03-20 09:53:36 +0900

    update: メニューアイテムのIconをdivで囲いました。

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -76,7 +76,7 @@ export const SideBar = () => {
           >
             <Link href={menuItemProps.href}>
               <div className="flex items-center h-4w text-white text-1.25">
-                {menuItemProps.icon}
+                <div>{menuItemProps.icon}</div>
                 {menuItemProps.name}
               </div>
             </Link>
