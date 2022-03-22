2daa1d6db037e27214b8fe99100079e8bb9b1e09
Umiteru2004 <umiteru2004@gmail.com>
2022-03-22 11:14:20 +0900

    update: Iconの対応するメニューを探索するswitchにcaseを追加しました。

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -6,12 +6,20 @@ interface Props {
 export const Icon = ({ chosen, isChose }: Props) => {
   switch (chosen) {
     case "home":
+      return;
     case "menu":
+      return;
     case "monitoring":
+      return;
     case "order":
+      return;
     case "sales":
+      return;
     case "settings":
+      return;
     case "shift":
+      return;
     case "user":
+      return;
   }
 };
