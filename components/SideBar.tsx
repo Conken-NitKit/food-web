c59a6657b632899c018be4fcc225b3593ff9f8eb
Umiteru2004 <umiteru2004@gmail.com>
2022-03-22 22:07:59 +0900

    rename: メニューアイテムを格納するオブジェクト配列定数のプロパティhref→idに改名しました。

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -13,35 +13,35 @@ import {
 
 const MENUS_ITEMS = [
   {
-    href: "home",
+    id: "home",
     name: "ホーム",
   },
   {
-    href: "menu",
+    id: "menu",
     name: "メニュー",
   },
   {
-    href: "order",
+    id: "order",
     name: "注文状況",
   },
   {
-    href: "sales",
+    id: "sales",
     name: "売上解析",
   },
   {
-    href: "shift",
+    id: "shift",
     name: "シフト",
   },
   {
-    href: "user",
+    id: "user",
     name: "ユーザー",
   },
   {
-    href: "monitoring",
+    id: "monitoring",
     name: "モニタリング",
   },
   {
-    href: "settings",
+    id: "settings",
     name: "設定",
   },
 ];
@@ -59,15 +59,15 @@ export const SideBar = ({ chosen }: Props) => {
       <ul>
         {MENUS_ITEMS.map((menuItems) => (
           <li
-            key={menuItems.href}
+            key={menuItems.id}
             className={`pl-9p h-4w${
-              menuItems.href === chosen && " rounded-l-1 bg-white"
+              menuItems.id === chosen && " rounded-l-1 bg-white"
             }`}
           >
-            <Link href={menuItems.href}>
+            <Link href={menuItems.id}>
               <div
                 className={`flex items-center h-4w text-1.25 font-bold ${
-                  menuItems.href === chosen ? "text-m" : "text-white" //text-をスコープ外に出すとtailwind.configの更新時に色が反映されない不具合を確認しました。
+                  menuItems.id === chosen ? "text-m" : "text-white" //text-をスコープ外に出すとtailwind.configの更新時に色が反映されない不具合を確認しました。
                 }`}
               >
                 <div className="mr-6p w-2w h-1/2"></div>
