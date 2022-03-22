ed589c46930cb373a0fcb7d0541822292e343b18
平田海輝 <seaglory2004@gmail.com>
2022-03-22 11:47:46 +0900

    update: 全てのアイコンのpropsの選択中のメニューを格納するプロパティを選択の有無を示すプロパティに変更しました。

--- components/icons/MonitoringIcon.tsx
+++ components/icons/MonitoringIcon.tsx
@@ -1,6 +1,6 @@
 import { IconProps } from "../../interfaces";
 
-export const MonitoringIcon = ({ chosen }: IconProps) => {
+export const MonitoringIcon = ({ isChose }: IconProps) => {
   return (
     <svg viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
