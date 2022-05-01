14160243308bfe0f940b0bd8b4f15982c9fe9449
BuildTools <seaglory2004@gmail.com>
2022-05-02 07:51:34 +0900

    update: firebaseの構成オブジェクトを追加しました。

--- firebase.ts
+++ firebase.ts
@@ -2,3 +2,13 @@ import firebase from "firebase/app";
 import "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+});
