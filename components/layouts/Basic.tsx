226121b071a2272ad418c1161bf227b06c457362
BuildTools <seaglory2004@gmail.com>
2022-06-13 11:17:26 +0900

    update: BasicLayoutに画面いっぱいに広がるdivを追加しました。

--- components/layouts/Basic.tsx
+++ components/layouts/Basic.tsx
@@ -46,39 +46,41 @@ interface Props {
 
 export const BasicLayout = ({ chosen, children }: Props) => {
   return (
-    <div className="ml-5p pt-3p pl-2p w-23p h-full bg-gradient-to-t from-theme-start to-theme-end">
-      <p className="mb-13p font-bold text-1.8 text-white leading-1.172">
-        コンピュータ研究部
-      </p>
-      <ul>
-        {MENUS_ITEMS.map((menuItems) => (
-          <li
-            key={menuItems.id}
-            className={classNames(
-              "mt-7p first:mt-0 pl-9p h-4w cursor-pointer",
-              {
-                "rounded-l-1 bg-white": menuItems.id === chosen,
-              }
-            )}
-          >
-            <Link href={menuItems.id}>
-              <div
-                className={classNames(
-                  "flex items-center h-4w text-1.25 font-bold",
-                  menuItems.id === chosen
-                    ? "text-theme-sub text-theme"
-                    : "text-white" //"error: text-"を条件式の外に出すとtailwind.configの更新時にスタイルが反映されない不具合を確認しました。
-                )}
-              >
-                <div className="mr-6p w-2w h-1/2">
-                  <Icon id={menuItems.id} isChose={menuItems.id === chosen} />
+    <div>
+      <div className="ml-5p pt-3p pl-2p w-23p h-full bg-gradient-to-t from-theme-start to-theme-end">
+        <p className="mb-13p font-bold text-1.8 text-white leading-1.172">
+          コンピュータ研究部
+        </p>
+        <ul>
+          {MENUS_ITEMS.map((menuItems) => (
+            <li
+              key={menuItems.id}
+              className={classNames(
+                "mt-7p first:mt-0 pl-9p h-4w cursor-pointer",
+                {
+                  "rounded-l-1 bg-white": menuItems.id === chosen,
+                }
+              )}
+            >
+              <Link href={menuItems.id}>
+                <div
+                  className={classNames(
+                    "flex items-center h-4w text-1.25 font-bold",
+                    menuItems.id === chosen
+                      ? "text-theme-sub text-theme"
+                      : "text-white" //"error: text-"を条件式の外に出すとtailwind.configの更新時にスタイルが反映されない不具合を確認しました。
+                  )}
+                >
+                  <div className="mr-6p w-2w h-1/2">
+                    <Icon id={menuItems.id} isChose={menuItems.id === chosen} />
+                  </div>
+                  {menuItems.name}
                 </div>
-                {menuItems.name}
-              </div>
-            </Link>
-          </li>
-        ))}
-      </ul>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
       {children}
     </div>
   );
