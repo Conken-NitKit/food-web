19d737d101c9e5f858ad1f0b19fb1529ecd51436
BuildTools <seaglory2004@gmail.com>
2022-06-14 12:54:36 +0900

    update: タイトルを追加しました。

--- components/layouts/FeaturePage.tsx
+++ components/layouts/FeaturePage.tsx
@@ -1,3 +1,7 @@
 export const FeaturePageLayout = () => {
-  return <div className="p-5p pb-0 w-full h-full"></div>;
+  return (
+    <div className="p-5p pb-0 w-full h-full">
+      <p></p>
+    </div>
+  );
 };
