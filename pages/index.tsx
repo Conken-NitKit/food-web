5d2139b36ea0427f9d0f69d5467e833e41a8986d
BuildTools <seaglory2004@gmail.com>
2022-06-14 17:27:10 +0900

    update: index.tsxでBasicLayoutのimport先を変更しました。

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 
-import { BasicLayout } from "../components";
+import { BasicLayout } from "../components/layouts";
 import { FeaturePageLayout } from "../components/layouts/FeaturePage";
 
 const Home: NextPage = () => {
