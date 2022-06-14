3e71cce5d03b2cdc716ba702151f9628bb501f31
Umiteru2004 <umiteru2004@gmail.com>
2022-06-14 16:57:17 +0900

    update: タイトルを大きくしました。

--- components/layouts/FeaturePage.tsx
+++ components/layouts/FeaturePage.tsx
@@ -5,7 +5,7 @@ interface Props {
 export const FeaturePageLayout = ({ title }: Props) => {
   return (
     <div className="p-5p pb-0 w-full h-full">
-      <p className="text-2 font-bold">{title}</p>
+      <p className="text-2.25 font-bold">{title}</p>
     </div>
   );
 };
