8a52e1d457616b50e688cc0d1ded07fa74b1c913
BuildTools <seaglory2004@gmail.com>
2022-06-15 16:46:46 +0900

    update: menuItems.tsにメニューアイテム定数を追加しました。

--- constants/menusItems.ts
+++ constants/menusItems.ts
@@ -1,1 +1,34 @@
-export {};
+export const MENUS_ITEMS = [
+  {
+    id: "home",
+    name: "ホーム",
+  },
+  {
+    id: "menu",
+    name: "メニュー",
+  },
+  {
+    id: "order",
+    name: "注文状況",
+  },
+  {
+    id: "sales",
+    name: "売上解析",
+  },
+  {
+    id: "shift",
+    name: "シフト",
+  },
+  {
+    id: "user",
+    name: "ユーザー",
+  },
+  {
+    id: "monitoring",
+    name: "モニタリング",
+  },
+  {
+    id: "settings",
+    name: "設定",
+  },
+];
