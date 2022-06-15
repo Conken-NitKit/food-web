2c44444f24852da5a6983a49bda1aded2fd25af0
Umiteru2004 <umiteru2004@gmail.com>
2022-06-15 16:41:35 +0900

    update: propsの型の、タイトルを格納するプロパティの名前を変更しました。

--- components/layouts/FeaturePage.tsx
+++ components/layouts/FeaturePage.tsx
@@ -1,15 +1,15 @@
 import { BasicLayout } from "./Basic";
 
 interface Props {
-  title: string;
+  type: string;
 }
 
-export const FeaturePageLayout = ({ title }: Props) => {
+export const FeaturePageLayout = ({ type }: Props) => {
   return (
     <BasicLayout type="menu">
       <div className="p-5p pb-0 w-full h-full">
         <p className="mb-4p text-2.25 font-bold leading-1.172 text-letter">
-          {title}
+          {type}
         </p>
       </div>
     </BasicLayout>
