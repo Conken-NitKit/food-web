db8412ae9ae54c979cc233e28a803c33bc791944
BuildTools <seaglory2004@gmail.com>
2022-06-15 16:51:57 +0900

    update: BasicLayoutで、メニューアイテム定数をconstantsからimportしました。

--- components/layouts/Basic.tsx
+++ components/layouts/Basic.tsx
@@ -3,41 +3,7 @@ import Link from "next/link";
 import classNames from "classnames";
 
 import { Icon } from "..";
-
-const MENUS_ITEMS = [
-  {
-    id: "home",
-    name: "ホーム",
-  },
-  {
-    id: "menu",
-    name: "メニュー",
-  },
-  {
-    id: "order",
-    name: "注文状況",
-  },
-  {
-    id: "sales",
-    name: "売上解析",
-  },
-  {
-    id: "shift",
-    name: "シフト",
-  },
-  {
-    id: "user",
-    name: "ユーザー",
-  },
-  {
-    id: "monitoring",
-    name: "モニタリング",
-  },
-  {
-    id: "settings",
-    name: "設定",
-  },
-];
+import { MENUS_ITEMS } from "../../constants";
 
 interface Props {
   type: string;
