1a92267527f1ee202d9083cc45ae6b4869c5ab84
BuildTools <seaglory2004@gmail.com>
2022-06-15 17:29:53 +0900

    fix: 「keyがない」エラーの修正のためにFeaturePageLayout内のBasicLayoutにkeyを追加しました。

--- components/layouts/FeaturePage.tsx
+++ components/layouts/FeaturePage.tsx
@@ -11,7 +11,7 @@ export const FeaturePageLayout = ({ type }: Props) => {
       {MENUS_ITEMS.map(
         (menuItems) =>
           menuItems.id === type && (
-            <BasicLayout type={type}>
+            <BasicLayout type={type} key={type}>
               <div className="p-5p pb-0 w-full h-full">
                 <p className="mb-4p text-2.25 font-bold leading-1.172 text-letter">
                   {menuItems.name}
