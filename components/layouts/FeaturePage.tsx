b28717e03e033e0d965775e0bc9fa24b5125a454
Umiteru2004 <umiteru2004@gmail.com>
2022-06-15 17:25:50 +0900

    fix: 文法エラーを修正しました。

--- components/layouts/FeaturePage.tsx
+++ components/layouts/FeaturePage.tsx
@@ -8,17 +8,18 @@ interface Props {
 export const FeaturePageLayout = ({ type }: Props) => {
   return (
     <>
-      {MENUS_ITEMS.map((menuItems) => {
-        menuItems.id === type && (
-          <BasicLayout type={type}>
-            <div className="p-5p pb-0 w-full h-full">
-              <p className="mb-4p text-2.25 font-bold leading-1.172 text-letter">
-                {menuItems.name}
-              </p>
-            </div>
-          </BasicLayout>
-        );
-      })}
+      {MENUS_ITEMS.map(
+        (menuItems) =>
+          menuItems.id === type && (
+            <BasicLayout type={type}>
+              <div className="p-5p pb-0 w-full h-full">
+                <p className="mb-4p text-2.25 font-bold leading-1.172 text-letter">
+                  {menuItems.name}
+                </p>
+              </div>
+            </BasicLayout>
+          )
+      )}
     </>
   );
 };
