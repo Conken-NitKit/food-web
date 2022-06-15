57db08b13d8157ec7df0cfd458fe9f6807e3ced7
BuildTools <seaglory2004@gmail.com>
2022-06-15 16:09:40 +0900

    update: FeaturePageLayoutをBasicLayoutの入れ子にしました。

--- components/layouts/FeaturePage.tsx
+++ components/layouts/FeaturePage.tsx
@@ -1,13 +1,17 @@
+import { BasicLayout } from "./Basic";
+
 interface Props {
   title: string;
 }
 
 export const FeaturePageLayout = ({ title }: Props) => {
   return (
-    <div className="p-5p pb-0 w-full h-full">
-      <p className="mb-4p text-2.25 font-bold leading-1.172 text-letter">
-        {title}
-      </p>
-    </div>
+    <BasicLayout chosen="menu">
+      <div className="p-5p pb-0 w-full h-full">
+        <p className="mb-4p text-2.25 font-bold leading-1.172 text-letter">
+          {title}
+        </p>
+      </div>
+    </BasicLayout>
   );
 };
