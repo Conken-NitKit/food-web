366679b1fb689c4d9f25adcb2785a6e71efb2d80
Umiteru2004 <umiteru2004@gmail.com>
2022-06-20 08:26:32 +0900

    update: ページの構造を変更しました。

--- components/layouts/Feature.tsx
+++ components/layouts/Feature.tsx
@@ -14,21 +14,14 @@ export const FeatureLayout: NextPage<Props> = ({ type, children }) => {
   const pageName: string = MENUS_ITEMS[type].name;
 
   return (
-    <>
-      {MENUS_ITEMS.map(
-        (menuItems) =>
-          menuItems.id === type && (
-            <BasicLayout type={type} key={type}>
-              <div className="p-5p pb-0 w-full h-full">
-                <h1 className="mb-4p text-2.25 font-bold leading-1.172 text-letter">
-                  {menuItems.name}
-                </h1>
+    <BasicLayout type={type}>
+      <div className="p-5p pb-0 w-full h-full">
+        <h1 className="mb-4p text-2.25 font-bold leading-1.172 text-letter">
+          {pageName}
+        </h1>
 
-                {children}
-              </div>
-            </BasicLayout>
-          )
-      )}
-    </>
+        {children}
+      </div>
+    </BasicLayout>
   );
 };
