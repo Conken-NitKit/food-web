872260111ec037795f295775bd4916708535120f
BuildTools <seaglory2004@gmail.com>
2022-06-22 08:19:56 +0900

    rename: Icon.tsx → MenusIcons.tsx

--- components/menu-list-icons/index.ts
+++ components/menu-list-icons/index.ts
@@ -1,4 +1,4 @@
-export { Icon } from "./Icon";
+export { Icon } from "./MenusIcons";
 export { HomeIcon } from "./HomeIcon";
 export { MenuIcon } from "./MenuIcon";
 export { OrderIcon } from "./OrderIcon";
