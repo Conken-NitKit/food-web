f89ce00c84bc0d1cfde300b01ba7dd276fce39ed
BuildTools <seaglory2004@gmail.com>
2022-06-22 08:05:12 +0900

    move: /components/Icon.tsx → /components/icons/Icon.tsx

--- components/icons/Icon.tsx
+++ components/icons/Icon.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from "next";
 
-import { IconProps, MenuId } from "../types";
+import { IconProps, MenuId } from "../../types";
 import {
   HomeIcon,
   MenuIcon,
@@ -10,7 +10,7 @@ import {
   SettingsIcon,
   ShiftIcon,
   UserIcon,
-} from "./icons";
+} from ".";
 
 const icons: Record<MenuId, (props: IconProps) => JSX.Element> = {
   home: (props) => <HomeIcon {...props} />,
