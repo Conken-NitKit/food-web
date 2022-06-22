244dcb391fe6a9d1acc47b1fb87548bb6e12515b
BuildTools <seaglory2004@gmail.com>
2022-06-23 07:44:53 +0900

    update: Layoutコンポーネントのpropsの型を/typesからexportしました。

--- components/layouts/Basic.tsx
+++ components/layouts/Basic.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import classNames from "classnames";
 
-import { LayoutProps } from "../../types/LayoutProps";
+import { LayoutProps } from "../../types";
 import { MenuItemIcon } from "..";
 import { MENUS_ITEMS } from "../../constants";
 
