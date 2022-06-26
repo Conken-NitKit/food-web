8823e1bb962f1dfc04bd6183344d15a7a6b83460
Umiteru2004 <umiteru2004@gmail.com>
2022-06-27 07:33:56 +0900

    update: 注文状況ページの大枠を作成しました。

--- pages/orders.tsx
+++ pages/orders.tsx
@@ -1,1 +1,12 @@
-export {};
+import { NextPage } from "next";
+import { FeatureLayout } from "../components/layouts";
+
+const Orders: NextPage = () => {
+  return (
+    <FeatureLayout type="orders">
+      <></>
+    </FeatureLayout>
+  );
+};
+
+export default Orders;
