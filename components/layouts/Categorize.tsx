497943db7a40fa8a9ff505a9bc02a99d7067dfd8
Umiteru2004 <umiteru2004@gmail.com>
2022-06-27 12:45:59 +0900

    update: CategorizeLayoutをdefault export → named exportに変更しました。

--- components/layouts/Categorize.tsx
+++ components/layouts/Categorize.tsx
@@ -1,12 +1,10 @@
 import { NextPage } from "next";
 import { FeatureLayout } from ".";
 
-const Orders: NextPage = () => {
+export const Orders: NextPage = () => {
   return (
     <FeatureLayout type="orders">
       <></>
     </FeatureLayout>
   );
 };
-
-export default Orders;
