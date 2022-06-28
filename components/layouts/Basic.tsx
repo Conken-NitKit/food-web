1c912fc6f3f63177e67fe329722cfb342c4d67fc
Umiteru2004 <umiteru2004@gmail.com>
2022-06-28 18:29:24 +0900

    update: スタイル一部をTailwind JITに変更しました。

--- components/layouts/Basic.tsx
+++ components/layouts/Basic.tsx
@@ -11,28 +11,28 @@ export const BasicLayout: ({ type, children }: LayoutProps) => JSX.Element = ({
 }) => {
   return (
     <div className="flex w-screen h-screen">
-      <div className="ml-20 pl-8 w-360px h-full bg-gradient-to-t from-theme-start to-theme-end">
-        <h2 className="my-13% text-28px text-white font-bold leading-1.17">
+      <div className="ml-20 pl-8 w-[360px] h-full bg-gradient-to-t from-theme-start to-theme-end">
+        <h2 className="my-[13%] text-[28px] text-white font-bold leading-1.17">
           コンピュータ研究部
         </h2>
         <ul>
           {Object.values(MENUS_ITEMS).map((menuItems) => (
             <li
               key={menuItems.id}
-              className={classNames("mt-7% first:mt-0 h-16 cursor-pointer", {
+              className={classNames("mt-[7%] first:mt-0 h-16 cursor-pointer", {
                 "rounded-l-2xl bg-white": menuItems.id === type,
               })}
             >
               <Link href={menuItems.id}>
                 <div
                   className={classNames(
-                    "flex items-center pl-9% h-full",
+                    "flex items-center pl-[9%] h-full",
                     menuItems.id === type
                       ? "text-theme-sub text-theme"
                       : "text-white"
                   )}
                 >
-                  <div className="w-11%">
+                  <div className="w-[11%]">
                     <MenuItemIcon
                       id={menuItems.id}
                       isChose={menuItems.id === type}
