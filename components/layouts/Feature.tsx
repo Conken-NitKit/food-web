a42eb1996dda70420bbd4ff8ea9aeaf70fbca236
Umiteru2004 <umiteru2004@gmail.com>
2022-06-30 18:30:05 +0900

    update: ページタイトルの大きさをレスポンシブ対応しました。

--- components/layouts/Feature.tsx
+++ components/layouts/Feature.tsx
@@ -11,7 +11,7 @@ export const FeatureLayout: ({
   return (
     <BasicLayout type={type}>
       <div className="px-[19px] md:px-[23px] lg:px-[31px] xl:px-[38px] 2xl:px-[46px] pb-0 w-full h-full">
-        <h1 className="mb-12 text-letter text-4xl font-bold leading-basic">
+        <h1 className="my-[19px] md:my-[23px] lg:my-[31px] xl:my-[38px] 2xl:my-[46px] text-letter text-sm md:text-[17px] lg:text-[23px] xl:text-[29px] 2xl:text-[35px] font-bold leading-basic">
           {pageName}
         </h1>
 
