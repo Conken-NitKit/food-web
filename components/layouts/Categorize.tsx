aef96af811c7ed271e4908c9e3515a24bbfd5c11
Umiteru2004 <umiteru2004@gmail.com>
2022-07-02 08:29:27 +0900

    update: CategorizeLayoutにカテゴリリストのulを追加しました。

--- components/layouts/Categorize.tsx
+++ components/layouts/Categorize.tsx
@@ -5,5 +5,10 @@ export const CategorizeLayout: ({
   type,
   children,
 }: LayoutProps) => JSX.Element = ({ type, children }) => {
-  return <FeatureLayout type={type}>{children}</FeatureLayout>;
+  return (
+    <FeatureLayout type={type}>
+      <ul className="flex h-[18px] md:h-[22px] lg:h-[29px] xl:h-[37px] 2xl:h-11"></ul>
+      {children}
+    </FeatureLayout>
+  );
 };
