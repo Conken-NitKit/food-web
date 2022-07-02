10f4862e6f076d2958b81d3b0e9dd4395fa19ec0
Umiteru2004 <umiteru2004@gmail.com>
2022-07-02 09:04:41 +0900

    rename: MenuItemIcon.tsxにページIDの型の改名を反映しました。

--- components/MenuItemIcon.tsx
+++ components/MenuItemIcon.tsx
@@ -1,4 +1,4 @@
-import { IconProps, MenuId } from "../types";
+import { IconProps, PageId } from "../types";
 import {
   HomeIcon,
   MenuIcon,
@@ -10,7 +10,7 @@ import {
   UserIcon,
 } from "./icons";
 
-const icons: Record<MenuId, (props: IconProps) => JSX.Element> = {
+const icons: Record<PageId, (props: IconProps) => JSX.Element> = {
   home: (props) => <HomeIcon {...props} />,
   menu: (props) => <MenuIcon {...props} />,
   monitoring: (props) => <MonitoringIcon {...props} />,
@@ -22,7 +22,7 @@ const icons: Record<MenuId, (props: IconProps) => JSX.Element> = {
 };
 
 interface Props {
-  id: MenuId;
+  id: PageId;
   isChose: boolean;
 }
 
