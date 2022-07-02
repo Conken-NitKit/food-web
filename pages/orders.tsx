03287bfe03da9b2fcb0a01e9b39383913c74b156
Umiteru2004 <umiteru2004@gmail.com>
2022-07-03 07:51:28 +0900

    update: 注文状況ページを追加しました。

--- pages/orders.tsx
+++ pages/orders.tsx
@@ -1,1 +1,12 @@
-export {};
+import { NextPage } from "next";
+import { CategorizeLayout } from "../components/layouts/Categorize";
+
+const Orders: NextPage = () => {
+  return (
+    <CategorizeLayout type="orders">
+      <></>
+    </CategorizeLayout>
+  );
+};
+
+export default Orders;
