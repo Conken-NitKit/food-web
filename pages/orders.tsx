960339db2724e471d67c6b43dbeb33b7e860704e
Umiteru2004 <umiteru2004@gmail.com>
2022-07-03 08:18:03 +0900

    update: 注文状況カテゴリ定数を追加しました。

--- pages/orders.tsx
+++ pages/orders.tsx
@@ -1,5 +1,29 @@
 import { NextPage } from "next";
+
+import { OrderCategoryId } from "../types";
 import { CategorizeLayout } from "../components/layouts/Categorize";
+
+const ORDER_CATEGORIES: Record<
+  OrderCategoryId,
+  { id: OrderCategoryId; name: string }
+> = {
+  all: {
+    id: "all",
+    name: "すべて",
+  },
+  canceled: {
+    id: "canceled",
+    name: "すべて",
+  },
+  done: {
+    id: "done",
+    name: "すべて",
+  },
+  waiting: {
+    id: "waiting",
+    name: "すべて",
+  },
+};
 
 const Orders: NextPage = () => {
   return (
