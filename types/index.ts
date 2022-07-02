0af51de5bee2de820ad397880a88de9fe6ba4c35
Umiteru2004 <umiteru2004@gmail.com>
2022-07-03 08:26:52 +0900

    update: 注文カテゴリ定数の型を/typesからexportしました。

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,7 @@
+export type { PageId } from "./PageId";
+export type { OrderCategoryId } from "./OrderCategoryId";
+
 export type { LayoutProps } from "./LayoutProps";
 export type { IconProps } from "./IconProps";
 
-export type { PageId } from "./PageId";
-export type { OrderCategoryId } from "./OrderCategoryId";
+export type { OrderCategories } from "./OrderCategories";
