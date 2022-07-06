66943bd72f52dc58ae1890387f32ce401d14250b
Umiteru2004 <umiteru2004@gmail.com>
2022-07-06 13:05:35 +0900

    update: CategorizeLayoutのpropsにカテゴリを格納するプロパティを追加しました。

--- components/layouts/Categorize.tsx
+++ components/layouts/Categorize.tsx
@@ -9,7 +9,7 @@ export const CategorizeLayout: ({
   type,
   children,
   categories,
-}: CategorizeLayoutProps) => JSX.Element = ({ type, children }) => {
+}: CategorizeLayoutProps) => JSX.Element = ({ type, children, categories }) => {
   return (
     <FeatureLayout type={type}>
       <ul className="flex h-[18px] md:h-[22px] lg:h-[29px] xl:h-[37px] 2xl:h-11"></ul>
