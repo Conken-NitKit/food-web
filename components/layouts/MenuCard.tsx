076aeb15fbaea6e248d61f756c2def4ebdedf7fa
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-07-08 14:23:33 +0900

    add: MenuCardに商品名を追加

--- components/layouts/MenuCard.tsx
+++ components/layouts/MenuCard.tsx
@@ -7,7 +7,13 @@ export const MenuCard: () => JSX.Element = () => {
         src="https://www.freshnessburger.co.jp/images/menu/thumb/classic_burger.jpg?ver=220316_000357"
         className="h-[75px] md:h-[90px] lg:h-[120px] xl:h-[150px] 2xl:h-[180px] w-[100%]"
       />
-      <div className="mx-auto w-[106px] md:w-[127px] lg:w-[169px] xl:w-[211px] 2xl:w-[253px] h-[48px] md:h-[58px] lg:h-[77px] xl:h-[97px] 2xl:h-[116px]"></div>
+      <div className="mx-auto w-[106px] md:w-[127px] lg:w-[169px] xl:w-[211px] 2xl:w-[253px] h-[48px] md:h-[58px] lg:h-[77px] xl:h-[97px] 2xl:h-[116px]">
+        <div className="flex items-center h-[7px] md:h-[9px] lg:h-[12px] xl:h-[14px] 2xl:h-[17px] mt-[5px] md:mt-[6px] lg:mt-[8px] xl:mt-[10px] 2xl:mt-[12px] font-bold">
+          <p className="text-[7px] md:text-[9px] lg:text-[12px] xl:text-[14px] 2xl:text-[17px]">
+            ビーフハンバーガー
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
