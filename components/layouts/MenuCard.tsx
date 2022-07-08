4ef91a6c6e61892beeafa00912784bf06868f109
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-07-08 14:19:46 +0900

    add: MenuCardにイメージ画像を追加（画像は仮置き）

--- components/layouts/MenuCard.tsx
+++ components/layouts/MenuCard.tsx
@@ -1,5 +1,12 @@
-export const MenuCard: () => JSX.Element = ({}) => {
+import { LayoutProps } from "../../types";
+
+export const MenuCard: () => JSX.Element = () => {
   return (
-    <div className="overflow-hidden shadow-md float-left w-[118px] md:w-[142px] lg:w-[189px] xl:w-[237px] 2xl:w-[284px] rounded-[10px] md:rounded-[12px] lg:rounded-[15px] xl:rounded-[19px] 2xl:rounded-[23px] m-[19px] md:m-[23px] lg:m-[31px] xl:m-[38px] 2xl:m-[46px]"></div>
+    <div className="overflow-hidden shadow-md float-left w-[118px] md:w-[142px] lg:w-[189px] xl:w-[237px] 2xl:w-[284px] rounded-[10px] md:rounded-[12px] lg:rounded-[15px] xl:rounded-[19px] 2xl:rounded-[23px] m-[19px] md:m-[23px] lg:m-[31px] xl:m-[38px] 2xl:m-[46px]">
+      <img
+        src="https://www.freshnessburger.co.jp/images/menu/thumb/classic_burger.jpg?ver=220316_000357"
+        className="h-[75px] md:h-[90px] lg:h-[120px] xl:h-[150px] 2xl:h-[180px] w-[100%]"
+      />
+    </div>
   );
 };
