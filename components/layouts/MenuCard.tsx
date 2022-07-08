b70f202e97c032e0fcace4b6208c0509be2b15e3
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-07-08 14:14:04 +0900

    add: MenuCardの枠を作成

--- components/layouts/MenuCard.tsx
+++ components/layouts/MenuCard.tsx
@@ -1,10 +1,5 @@
-import { LayoutProps } from "../../types";
-import { FeatureLayout } from "./Feature";
-
-export const MenuCard: () => JSX.Element = ({
-
-}) => {
+export const MenuCard: () => JSX.Element = ({}) => {
   return (
-    
+    <div className="overflow-hidden shadow-md float-left w-[118px] md:w-[142px] lg:w-[189px] xl:w-[237px] 2xl:w-[284px] rounded-[10px] md:rounded-[12px] lg:rounded-[15px] xl:rounded-[19px] 2xl:rounded-[23px] m-[19px] md:m-[23px] lg:m-[31px] xl:m-[38px] 2xl:m-[46px]"></div>
   );
 };
