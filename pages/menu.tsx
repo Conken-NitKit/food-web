0308786eec3adbf0d64acf044494460f6b2df46c
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-07-08 14:08:16 +0900

    add:pages/menuの追加

---
+++ pages/menu.tsx
@@ -0,0 +1,10 @@
+const Menu: NextPage = () => {
+  return (
+    <div className="w-screen h-screen">
+      <FeatureLayout type="menu">
+      </FeatureLayout>
+    </div>
+  );
+};
+
+export default Menu;
