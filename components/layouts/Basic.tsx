1e2db7675bc4194fc308a18a7341e3636211ee3b
Umiteru2004 <umiteru2004@gmail.com>
2022-07-08 11:40:31 +0900

    remove: メニューバースペースとメインスペースの幅のクラスを格納する定数の、明示的な型指定を削除しました。

--- components/layouts/Basic.tsx
+++ components/layouts/Basic.tsx
@@ -11,17 +11,17 @@ const MENU_BAR_W_LG = 282;
 const MENU_BAR_W_XL = 352;
 const MENU_BAR_W_2XL = 422;
 
-const menuBarWClass: string = "w-[" + MENU_BAR_W + "px]";
-const menuBarWClassMd: string = "md:w-[" + MENU_BAR_W_MD + "px]";
-const menuBarWClassLg: string = "lg:w-[" + MENU_BAR_W_LG + "px]";
-const menuBarWClassXl: string = "xl:w-[" + MENU_BAR_W_XL + "px]";
-const menuBarWClass2xl: string = "2xl:w-[" + MENU_BAR_W_2XL + "px]";
+const menuBarWClass = "w-[" + MENU_BAR_W + "px]";
+const menuBarWClassMd = "md:w-[" + MENU_BAR_W_MD + "px]";
+const menuBarWClassLg = "lg:w-[" + MENU_BAR_W_LG + "px]";
+const menuBarWClassXl = "xl:w-[" + MENU_BAR_W_XL + "px]";
+const menuBarWClass2xl = "2xl:w-[" + MENU_BAR_W_2XL + "px]";
 
-const mainWClass: string = "w-[calc(100%-" + MENU_BAR_W + "px)]";
-const mainWClassMd: string = "md:w-[calc(100%-" + MENU_BAR_W_MD + "px)]";
-const mainWClassLg: string = "lg:w-[calc(100%-" + MENU_BAR_W_LG + "px)]";
-const mainWClassXl: string = "xl:w-[calc(100%-" + MENU_BAR_W_XL + "px)]";
-const mainWClass2xl: string = "2xl:w-[calc(100%-" + MENU_BAR_W_2XL + "px)]";
+const mainWClass = "w-[calc(100%-" + MENU_BAR_W + "px)]";
+const mainWClassMd = "md:w-[calc(100%-" + MENU_BAR_W_MD + "px)]";
+const mainWClassLg = "lg:w-[calc(100%-" + MENU_BAR_W_LG + "px)]";
+const mainWClassXl = "xl:w-[calc(100%-" + MENU_BAR_W_XL + "px)]";
+const mainWClass2xl = "2xl:w-[calc(100%-" + MENU_BAR_W_2XL + "px)]";
 
 export const BasicLayout: ({ type, children }: LayoutProps) => JSX.Element = ({
   type,
