b442d24711dcdf81ab0edf16ce4c098a8a279309
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-07-08 15:38:05 +0900

    fix: IsSoldをisSoldに再変更

--- components/layouts/MenuCard.tsx
+++ components/layouts/MenuCard.tsx
@@ -1,4 +1,4 @@
-export const MenuCard: ({ IsSold: boolean }) => JSX.Element = ({ IsSold }) => {
+export const MenuCard: ({ isSold: boolean }) => JSX.Element = ({ isSold }) => {
   return (
     <div className="overflow-hidden shadow-md float-left shadow-md m-[19px] md:m-[23px] lg:m-[31px] xl:m-[38px] 2xl:m-[46px] w-[118px] md:w-[142px] lg:w-[189px] xl:w-[237px] 2xl:w-[284px] rounded-[10px] md:rounded-[12px] lg:rounded-[15px] xl:rounded-[19px] 2xl:rounded-[23px]">
       <img
@@ -10,7 +10,7 @@ export const MenuCard: ({ IsSold: boolean }) => JSX.Element = ({ IsSold }) => {
           <p className="text-[7px] md:text-[9px] lg:text-[12px] xl:text-[14px] 2xl:text-[17px]">
             ビーフハンバーガー
           </p>
-          {IsSold && (
+          {isSold && (
             <div className="overflow-scroll text-center bg-[#E8521E] py-[1px] md:py-[1px] lg:py-[2px] xl:py-[2px] 2xl:py-[2px] ml-[auto] w-[22px] md:w-[27px] lg:w-[36px] xl:w-[45px] 2xl:w-[54px]  rounded-[2px] md:rounded-[2px] lg:rounded-[3px] xl:rounded-[3px] 2xl:rounded-[4px] text-[4px] md:text-[5px] lg:text-[6px] xl:text-[8px] 2xl:text-[10px]">
               売り切れ
             </div>
