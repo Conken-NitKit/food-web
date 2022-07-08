1c679c3f6ced7aff4af5cc28d4ba69dbc811b356
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-07-08 14:12:30 +0900

    add: pages/menuにmenuCardをインポート

--- pages/menu.tsx
+++ pages/menu.tsx
@@ -1,7 +1,10 @@
+import { MenuCard } from "../components/layouts";
+
 const Menu: NextPage = () => {
   return (
     <div className="w-screen h-screen">
       <FeatureLayout type="menu">
+        <MenuCard />
       </FeatureLayout>
     </div>
   );
