29368371d5d176b41e73708457efa4c316fb201b
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-07-08 19:55:39 +0900

    style: MenuCardのテキストのカラーを変更

--- components/MenuCard.tsx
+++ components/MenuCard.tsx
@@ -5,7 +5,7 @@ export const MenuCard: ({ isSold: boolean }) => JSX.Element = ({ isSold }) => {
         src="https://www.freshnessburger.co.jp/images/menu/thumb/classic_burger.jpg?ver=220316_000357"
         className="w-[100%] h-[75px] md:h-[90px] lg:h-[120px] xl:h-[150px] 2xl:h-[180px]"
       />
-      <div className="mx-auto w-[106px] md:w-[127px] lg:w-[169px] xl:w-[211px] 2xl:w-[253px] h-[48px] md:h-[58px] lg:h-[77px] xl:h-[97px] 2xl:h-[116px]">
+      <div className="text-letter mx-auto w-[106px] md:w-[127px] lg:w-[169px] xl:w-[211px] 2xl:w-[253px] h-[48px] md:h-[58px] lg:h-[77px] xl:h-[97px] 2xl:h-[116px]">
         <div className="flex items-center font-bold mt-[5px] md:mt-[6px] lg:mt-[8px] xl:mt-[10px] 2xl:mt-[12px] h-[7px] md:h-[9px] lg:h-[12px] xl:h-[14px] 2xl:h-[17px]">
           <p className="text-[7px] md:text-[9px] lg:text-[12px] xl:text-[14px] 2xl:text-[17px]">
             ビーフハンバーガー
