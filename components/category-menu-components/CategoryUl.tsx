f3082244956907d9d0d33b4440acd5fa882504e1
Umiteru2004 <umiteru2004@gmail.com>
2022-07-11 08:03:07 +0900

    update: カテゴリメニューulを明示的に型指定しました。

--- components/category-menu-components/CategoryUl.tsx
+++ components/category-menu-components/CategoryUl.tsx
@@ -4,7 +4,9 @@ interface Props {
   children: ReactNode;
 }
 
-export const CategoryUl = ({ children }: Props) => {
+export const CategoryUl: ({ children }: Props) => JSX.Element = ({
+  children,
+}: Props) => {
   return (
     <ul className="flex h-[18px] md:h-[22px] lg:h-[29px] xl:h-[37px] 2xl:h-11">
       {children}
