8c117e23e9e1a250112e36ffe67f7374d25dcad2
Umiteru2004 <umiteru2004@gmail.com>
2022-07-11 07:50:42 +0900

    update: カテゴリメニュースペースコンポーネントのpropsにchildrenを渡しました。

--- components/category-menu-components/CategoryUl.tsx
+++ components/category-menu-components/CategoryUl.tsx
@@ -4,7 +4,7 @@ interface Props {
   children: ReactNode;
 }
 
-export const CategoryUl = () => {
+export const CategoryUl = ({ children }: Props) => {
   return (
     <ul className="flex h-[18px] md:h-[22px] lg:h-[29px] xl:h-[37px] 2xl:h-11"></ul>
   );
