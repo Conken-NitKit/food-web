66405a191d389b0ff192966c0b7828680d37619f
Umiteru2004 <umiteru2004@gmail.com>
2022-07-11 07:51:50 +0900

    update: カテゴリメニュースペースコンポーネントにchildrenを追加しました。

--- components/category-menu-components/CategoryUl.tsx
+++ components/category-menu-components/CategoryUl.tsx
@@ -6,6 +6,8 @@ interface Props {
 
 export const CategoryUl = ({ children }: Props) => {
   return (
-    <ul className="flex h-[18px] md:h-[22px] lg:h-[29px] xl:h-[37px] 2xl:h-11"></ul>
+    <ul className="flex h-[18px] md:h-[22px] lg:h-[29px] xl:h-[37px] 2xl:h-11">
+      {children}
+    </ul>
   );
 };
