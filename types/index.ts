0c5dbcaa69c770b971b56059dac6667f6395ca7d
Umiteru2004 <umiteru2004@gmail.com>
2022-07-11 08:10:01 +0900

    remove: /typesから注文カテゴリのデータの型のexportを削除しました。

--- types/index.ts
+++ types/index.ts
@@ -3,5 +3,3 @@ export type { OrderCategoryId } from "./OrderCategoryId";
 
 export type { LayoutProps } from "./LayoutProps";
 export type { IconProps } from "./IconProps";
-
-export type { OrderCategories } from "./OrderCategories";
