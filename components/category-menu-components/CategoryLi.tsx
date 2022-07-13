4fcefdd682d4358c17fef8746f410b542ca15146
Umiteru2004 <umiteru2004@gmail.com>
2022-07-13 21:25:07 +0900

    update: カテゴリメニューliのテキストをpの入れ子にしました。

--- components/category-menu-components/CategoryLi.tsx
+++ components/category-menu-components/CategoryLi.tsx
@@ -19,7 +19,7 @@ export const CategoryLi = ({ id, name, setSelectedCategory }: Props) => {
       }}
       className="flex justify-center items-center px-[5px] md:px-[6px] lg:px-2 xl:px-[10px] 2xl:px-3 h-full"
     >
-      {name}
+      <p>{name}</p>
     </li>
   );
 };
