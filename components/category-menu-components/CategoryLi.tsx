9a498c18538d9f2498bf521e584f975a2eebe62c
Umiteru2004 <umiteru2004@gmail.com>
2022-07-14 07:50:09 +0900

    style: カテゴリメニューliの文字に、カテゴリの選択時、非選択時のスタイルを追加しました。

--- components/category-menu-components/CategoryLi.tsx
+++ components/category-menu-components/CategoryLi.tsx
@@ -10,7 +10,12 @@ interface Props {
   setSelectedCategory: Dispatch<SetStateAction<OrderCategoryId>>;
 }
 
-export const CategoryLi = ({ id, name, setSelectedCategory }: Props) => {
+export const CategoryLi = ({
+  id,
+  name,
+  selectedCategory,
+  setSelectedCategory,
+}: Props) => {
   return (
     <li
       key={id}
@@ -19,7 +24,12 @@ export const CategoryLi = ({ id, name, setSelectedCategory }: Props) => {
       }}
       className="mr-5 md:mr-6 lg:mr-8 xl:mr-10 2xl:mr-12 h-full"
     >
-      <p className="flex justify-center items-center px-[5px] md:px-[6px] lg:px-2 xl:px-[10px] 2xl:px-3 h-[92%] text-[10px] md:text-xs lg:text-[15px] xl:text-[19px] 2xl:text-23[px]">
+      <p
+        className={classNames(
+          "flex justify-center items-center px-[5px] md:px-[6px] lg:px-2 xl:px-[10px] 2xl:px-3 h-[92%] text-[10px] md:text-xs lg:text-[15px] xl:text-[19px] 2xl:text-23[px]",
+          id === selectedCategory ? "text-letter font-bold" : "text-letter-sub"
+        )}
+      >
         {name}
       </p>
     </li>
