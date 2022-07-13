b266d4ba558fbe6f7f001862659df0666e41c36b
Umiteru2004 <umiteru2004@gmail.com>
2022-07-13 14:05:20 +0900

    update: カテゴリメニューliのpropsの型に選択されているカテゴリのIDのstateのsetStateActionを格納するプロパティを追加しました。

--- components/category-menu-components/CategoryLi.tsx
+++ components/category-menu-components/CategoryLi.tsx
@@ -1,8 +1,11 @@
+import { Dispatch, SetStateAction } from "react";
+
 import { OrderCategoryId } from "../../types";
 
 interface Props {
   key: OrderCategoryId;
   selectedCategory: OrderCategoryId;
+  setSelectedCategory: Dispatch<SetStateAction<OrderCategoryId>>;
   name: string;
 }
 
