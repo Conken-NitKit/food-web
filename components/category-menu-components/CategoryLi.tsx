4ec4c2745938cbd3d076958ad492239e977349ad
Umiteru2004 <umiteru2004@gmail.com>
2022-07-14 07:40:36 +0900

    style: カテゴリメニューliどうしの間隔を追加しました。

--- components/category-menu-components/CategoryLi.tsx
+++ components/category-menu-components/CategoryLi.tsx
@@ -17,7 +17,7 @@ export const CategoryLi = ({ id, name, setSelectedCategory }: Props) => {
       onClick={() => {
         setSelectedCategory(id);
       }}
-      className="h-full"
+      className="mr-5 md:mr-6 lg:mr-8 xl:mr-10 2xl:mr-12 h-full"
     >
       <p className="flex justify-center items-center px-[5px] md:px-[6px] lg:px-2 xl:px-[10px] 2xl:px-3 h-[92%] text-[10px] md:text-xs lg:text-[15px] xl:text-[19px] 2xl:text-23[px]">
         {name}
