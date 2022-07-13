d6ffc46f2e16477ac9ee8249cf7d95c9ccbfc46c
Umiteru2004 <umiteru2004@gmail.com>
2022-07-14 08:02:23 +0900

    update: カテゴリメニューliのカテゴリ選択・非選択時のスタイルを指定する条件式を、カテゴリが選択されているかどうかを示す変数に置換しました。

--- components/category-menu-components/CategoryLi.tsx
+++ components/category-menu-components/CategoryLi.tsx
@@ -29,7 +29,7 @@ export const CategoryLi = ({
       <p
         className={classNames(
           "flex justify-center items-center px-[5px] md:px-[6px] lg:px-2 xl:px-[10px] 2xl:px-3 h-[92%] text-[10px] md:text-xs lg:text-[15px] xl:text-[19px] 2xl:text-23[px]",
-          id === selectedCategory ? "text-letter font-bold" : "text-letter-sub"
+          isSelected ? "text-letter font-bold" : "text-letter-sub"
         )}
       >
         {name}
