a105fac7023b61173912692d93a2d4ac6a5ef344
平田海輝 <umiteru2004@gmail.com>
2022-07-15 07:50:33 +0900

    remove: カテゴリメニューliコンポーネントから、liタグを削除しました。

--- components/category-menu-components/CategoryLi.tsx
+++ components/category-menu-components/CategoryLi.tsx
@@ -19,12 +19,7 @@ export const CategoryLi = ({
   const isSelected = id === selectedCategory;
 
   return (
-    <li
-      onClick={() => {
-        setSelectedCategory(id);
-      }}
-      className="mr-5 md:mr-6 lg:mr-8 xl:mr-10 2xl:mr-12 h-full cursor-pointer"
-    >
+    <>
       <p
         className={classNames(
           "flex justify-center items-center px-[5px] md:px-[6px] lg:px-2 xl:px-[10px] 2xl:px-3 h-[92%] text-[10px] md:text-xs lg:text-[15px] xl:text-[19px] 2xl:text-23[px]",
@@ -37,6 +32,6 @@ export const CategoryLi = ({
       {isSelected && (
         <hr className="border-0 rounded-[1px] lg:rounded-sm h-[8%] bg-gradient-to-b from-theme-start to-theme-end" />
       )}
-    </li>
+    </>
   );
 };
