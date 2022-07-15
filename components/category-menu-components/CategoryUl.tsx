72156bcbaafa0afc810d16b9305a9641950c052d
平田海輝 <umiteru2004@gmail.com>
2022-07-15 18:31:42 +0900

    style: カテゴリメニューulの下に余白を追加しました。

--- components/category-menu-components/CategoryUl.tsx
+++ components/category-menu-components/CategoryUl.tsx
@@ -8,7 +8,7 @@ export const CategoryUl: ({ children }: Props) => JSX.Element = ({
   children,
 }: Props) => {
   return (
-    <ul className="flex h-[19px] md:h-[23px] lg:h-[30px] xl:h-[37px] 2xl:h-[46px]">
+    <ul className="flex mb-3 md:mb-3.5 lg:mb-[19px] xl:mb-6 2xl:mb-[29px] h-[19px] md:h-[23px] lg:h-[30px] xl:h-[37px] 2xl:h-[46px]">
       {children}
     </ul>
   );
