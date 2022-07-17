e7000dd0a1c299eeb03049cba4ff073b20819cbe
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-07-17 20:05:20 +0900

    add:  tailwind.configに新たに色を定義

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,10 @@ module.exports = {
         "theme-start": "#F2A649",
         "theme-end": "#F8714B",
         "theme-sub": "#F58B4A",
+        "sold-label": "#E8521E",
         letter: "#2D0D0D",
+        basic: "#FFDEB5",
+        negative: "#E5A3B0",
       },
 
       lineHeight: {
