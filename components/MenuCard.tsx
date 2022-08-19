6f667f6feb1d9ebb26bd661bf81a0d0d9ff89a5c
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-08-19 23:00:44 +0900

    style: MenuCardのスタイルの調整

--- components/MenuCard.tsx
+++ components/MenuCard.tsx
@@ -25,37 +25,32 @@ export const MenuCard: ({
   price,
 }) => {
   return (
-    <li className="overflow-hidden mt-[48px] ml-[38px] 2xl:ml-[46px] rounded-[10px] md:rounded-[12px] lg:rounded-[15px] xl:rounded-[19px] 2xl:rounded-[23px] w-[237px] 2xl:w-[284px] h-[247px] 2xl:h-[297px] shadow-md cursor-pointer">
+    <li className="overflow-hidden mt-[48px] 2xl:mt-[58px] ml-[38px] 2xl:ml-[46px] rounded-[23px] w-[237px] 2xl:w-[284px] h-[247px] 2xl:h-[297px] shadow-md cursor-pointer">
       <div
         className={classNames(
-          "flex items-center justify-center w-full h-[60%] text-[61px] ",
+          "flex items-center justify-center w-full h-[60.8%] text-[61px]",
           isSold ? "bg-sold-out" : "bg-on-sale"
         )}
       >
         {ideogram}
       </div>
-      <div className="mx-auto w-[90%] h-[40%] text-letter">
-        <div className="flex items-center pt-[5%] font-bold">
-          <p className="truncate w-[70%] text-[7px] md:text-[9px] lg:text-[12px] xl:text-[14px] 2xl:text-[17px]">
-            {products}
-          </p>
+      <div className="mx-auto pt-[4.1%] w-[89.2%] h-[39.2%] text-letter">
+        <div className="flex items-center font-bold">
+          <p className="truncate w-[70%] 2xl:text-[17px]">{products}</p>
           {isSold && (
-            <div className="ml-[auto] rounded-[2px] lg:rounded-[3px] 2xl:rounded-[4px] py-px w-[20%] 2xl:w-[54px] text-[63%] text-center bg-sold-label">
+            <div className="ml-[auto] rounded-[4px] px-[3.8%] py-[1.2%] text-[10px] text-center bg-sold-label">
               売り切れ
             </div>
           )}
         </div>
-        <p className="overflow-scroll py-[2.5%] h-[30%] text-[4px] md:text-[5px] lg:text-[6px] xl:text-[8px] 2xl:text-[10px] leading-3 break-all">
+        <p className="overflow-scroll mb-[2.5%] pt-[2.5%] h-[33%] text-[10px] leading-3 break-all">
           {promotion}
         </p>
-        <div className="whitespace-nowrap flex">
-          <p className="overflow-scroll w-[60%] text-[4px] md:text-[5px] lg:text-[6px] xl:text-[8px] 2xl:text-[10px] font-bold">
-            {SalesFormat} ￥
-            <span className="text-[8px] md:text-[10px] lg:text-[13px] xl:text-[16px] 2xl:text-[19px]">
-              {price}
-            </span>
+        <div className="flex items-center">
+          <p className="w-[60%] text-[10px] font-bold">
+            {SalesFormat} ￥<span className="text-[19px]">{price}</span>
           </p>
-          <button className="ml-[auto] rounded-[2px] lg:rounded-[3px] xl:rounded-[3px] 2xl:rounded-[4px] py-px w-[30%] text-[4px] md:text-[5px] lg:text-[6px] xl:text-[8px] 2xl:text-[10px] text-center bg-edit">
+          <button className="ml-[auto] rounded-[4px] py-[1%] w-[36.4%] text-[10px] text-center bg-edit">
             内容を編集
           </button>
         </div>
