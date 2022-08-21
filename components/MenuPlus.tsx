52c13ef3881b32f2ff6c160e151e5d206c48258d
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-08-21 16:54:33 +0900

    add: MenuPlusの＋ボタンの枠を作成

--- components/MenuPlus.tsx
+++ components/MenuPlus.tsx
@@ -1,6 +1,7 @@
 export const MenuPlus = () => {
   return (
     <li className="list-none mt-[48px] 2xl:mt-[58px] ml-[38px] 2xl:ml-[46px] rounded-[19px] 2xl:rounded-[23px] w-[237px] 2xl:w-[284px] h-[247px] 2xl:h-[297px] border 2xl:border-2 border-[#998888] border-dashed">
+      <button className="block mx-auto mt-[38.3%] w-[11%] h-[10.6%] rounded-full border-[3px] border-[#998888]"></button>
     </li>
   );
 };
