777c858d47ad4110f86d01e80090817cb6b1c621
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-08-23 22:55:01 +0900

    style: 十字ボタンのmarginを調整

--- components/MenuPlus.tsx
+++ components/MenuPlus.tsx
@@ -1,8 +1,8 @@
 export const MenuPlus = () => {
   return (
     <div className="mt-[48px] 2xl:mt-[58px] ml-[38px] 2xl:ml-[46px] border border-pale-black border-dashed rounded-[19px] 2xl:rounded-[23px] w-[237px] 2xl:w-[284px] h-[247px] 2xl:h-[297px]">
-      <div className="flex mt-[3%] mx-auto border-[3px] border-pale-black rounded-full w-[26px] 2xl:w-[32px] h-[26px] 2xl:h-[32px]">
-        <div className="m-auto w-[60%] h-[15%] bg-pale-black before:block before:rotate-90 before:w-[100%] before:h-[100%] before:content-[''] before:bg-pale-black"></p>
+      <div className="flex mt-[38.3%] mx-auto border-[3px] border-pale-black rounded-full w-[26px] 2xl:w-[32px] h-[26px] 2xl:h-[32px]">
+        <div className="m-auto w-[60%] h-[15%] bg-pale-black before:block before:rotate-90 before:w-[100%] before:h-[100%] before:content-[''] before:bg-pale-black" />
       </div>
       <p className="mx-auto mt-[6.3%] text-[16px] 2xl:text-[19px] text-pale-black text-center">
         メニューを追加
