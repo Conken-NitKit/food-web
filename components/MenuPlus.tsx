df25530ccc7a09fe623e03b91668fb08f30113f7
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-08-23 17:21:40 +0900

    style: MenuPlusのスタイルの調整

--- components/MenuPlus.tsx
+++ components/MenuPlus.tsx
@@ -1,12 +1,12 @@
 export const MenuPlus = () => {
   return (
-    <li className="list-none mt-[48px] 2xl:mt-[58px] ml-[38px] 2xl:ml-[46px] rounded-[19px] 2xl:rounded-[23px] w-[237px] 2xl:w-[284px] h-[247px] 2xl:h-[297px] border border-[#998888] border-dashed">
-      <button className="block mx-auto mt-[38.3%] h-[26px] 2xl:h-[32px] w-[26px] 2xl:w-[32px] rounded-full border-[3px] border-[#998888]">
-          <p className="m-auto relative w-[12px] h-[3px] bg-[#998888] before:block  before:content-[''] before:w-[12px] before:h-[3px] before:bg-[#998888] before:rotate-90"></p>
-      </button>
-      <p className="text-[16px] 2xl:text-[19px] text-[#998888] mx-auto mt-[6.3%] 2xl:mt-[18px] text-center">
+    <button className="mt-[48px] 2xl:mt-[58px] ml-[38px] 2xl:ml-[46px] border border-pale-black border-dashed rounded-[19px] 2xl:rounded-[23px] w-[237px] 2xl:w-[284px] h-[247px] 2xl:h-[297px]">
+      <div className="flex mt-[3%] mx-auto border-[3px] border-pale-black rounded-full w-[26px] 2xl:w-[32px] h-[26px] 2xl:h-[32px]">
+        <p className="m-auto w-[60%] h-[15%] bg-pale-black before:block before:rotate-90 before:w-[100%] before:h-[100%] before:content-[''] before:bg-pale-black"></p>
+      </div>
+      <p className="mx-auto mt-[6.3%] text-[16px] 2xl:text-[19px] text-pale-black text-center">
         メニューを追加
       </p>
-    </li>
+    </button>
   );
 };
