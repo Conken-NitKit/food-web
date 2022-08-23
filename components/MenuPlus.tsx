8e9162e14e2cf630769bd30ad2022ba3047eea45
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-08-23 12:22:25 +0900

    add: MenuPlusにプラスマークを追加

--- components/MenuPlus.tsx
+++ components/MenuPlus.tsx
@@ -1,7 +1,9 @@
 export const MenuPlus = () => {
   return (
-    <li className="list-none mt-[48px] 2xl:mt-[58px] ml-[38px] 2xl:ml-[46px] rounded-[19px] 2xl:rounded-[23px] w-[237px] 2xl:w-[284px] h-[247px] 2xl:h-[297px] border 2xl:border-2 border-[#998888] border-dashed">
-      <button className="block mx-auto mt-[38.3%] w-[11%] h-[10.6%] rounded-full border-[3px] border-[#998888]"></button>
+    <li className="list-none mt-[48px] 2xl:mt-[58px] ml-[38px] 2xl:ml-[46px] rounded-[19px] 2xl:rounded-[23px] w-[237px] 2xl:w-[284px] h-[247px] 2xl:h-[297px] border border-[#998888] border-dashed">
+      <button className="block mx-auto mt-[38.3%] h-[26px] 2xl:h-[32px] w-[26px] 2xl:w-[32px] rounded-full border-[3px] border-[#998888]">
+          <p className="m-auto relative w-[12px] h-[3px] bg-[#998888] before:block  before:content-[''] before:w-[12px] before:h-[3px] before:bg-[#998888] before:rotate-90"></p>
+      </button>
       <p className="text-[16px] 2xl:text-[19px] text-[#998888] mx-auto mt-[6.3%] 2xl:mt-[18px] text-center">
         メニューを追加
       </p>
