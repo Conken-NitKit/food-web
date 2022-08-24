ee2b765f535ef0730a71da12caaa755fad5ce88a
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-08-24 14:20:38 +0900

    update: MenuCardコンポーネントの型の定義方法を変更

--- components/MenuCard.tsx
+++ components/MenuCard.tsx
@@ -6,10 +6,7 @@ interface Props {
   isSold: boolean;
 }
 
-export const MenuCard: ({ content, isSold }: Props) => JSX.Element = ({
-  content,
-  isSold,
-}) => {
+export const MenuCard = ({ content, isSold }: Props): JSX.Element => {
   return (
     <div className="overflow-hidden mt-[48px] 2xl:mt-[58px] ml-[38px] 2xl:ml-[46px] rounded-[19px] 2xl:rounded-[23px] w-[237px] 2xl:w-[284px] h-[247px] 2xl:h-[297px] shadow-md cursor-pointer font-menu-card">
       <div
