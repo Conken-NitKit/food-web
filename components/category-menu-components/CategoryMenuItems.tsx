02f11b55430063dec7b868feed82f848a4cdb774
平田海輝 <umiteru2004@gmail.com>
2022-09-02 08:37:08 +0900

    update: カテゴリメニューアイテムコンポーネントの型指定を簡略化しました。

--- components/category-menu-components/CategoryMenuItems.tsx
+++ components/category-menu-components/CategoryMenuItems.tsx
@@ -5,10 +5,10 @@ interface Props {
   isSelected: boolean;
 }
 
-export const CategoryMenuItems: ({
+export const CategoryMenuItems = ({
   categoryName,
   isSelected,
-}: Props) => JSX.Element = ({ categoryName, isSelected }) => {
+}: Props): JSX.Element => {
   return (
     <div className="w-full h-full">
       <p
