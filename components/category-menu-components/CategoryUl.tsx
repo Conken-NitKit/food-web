cd1d47bcd9fdd5fbf9054189218f4df09992b257
平田海輝 <umiteru2004@gmail.com>
2022-09-02 08:36:26 +0900

    update: カテゴリulコンポーネントの型指定を簡略化しました。

--- components/category-menu-components/CategoryUl.tsx
+++ components/category-menu-components/CategoryUl.tsx
@@ -4,9 +4,7 @@ interface Props {
   children: ReactNode;
 }
 
-export const CategoryUl: ({ children }: Props) => JSX.Element = ({
-  children,
-}: Props) => {
+export const CategoryUl = ({ children }: Props): JSX.Element => {
   return (
     <ul className="flex mb-3 md:mb-3.5 lg:mb-[19px] xl:mb-6 2xl:mb-[29px] h-[19px] md:h-[23px] lg:h-[30px] xl:h-[37px] 2xl:h-[46px]">
       {children}
