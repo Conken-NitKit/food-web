66c5851bbab94fadac5f02d396e406b941bb0232
平田海輝 <umiteru2004@gmail.com>
2022-09-02 16:27:48 +0900

    update: 注文状況カテゴリの型を/typesからexportしました。

--- types/index.ts
+++ types/index.ts
@@ -3,3 +3,5 @@ export type { OrderCategoryId } from "./OrderCategoryId";
 
 export type { LayoutProps } from "./LayoutProps";
 export type { IconProps } from "./IconProps";
+
+export type { OrderCategory } from "./OrderCategory";
