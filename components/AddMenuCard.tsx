64d933ad7918f66bf02c71a50a071bb6cb2885e2
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-09-06 19:49:40 +0900

    fix: 適用されなくなったスタイルを修正

--- components/AddMenuCard.tsx
+++ components/AddMenuCard.tsx
@@ -2,7 +2,7 @@ export const AddMenuCard = (): JSX.Element => {
   return (
     <div className="mt-[48px] 2xl:mt-[58px] ml-[38px] 2xl:ml-[46px] border border-primary-regular border-dashed rounded-[19px] 2xl:rounded-[23px] w-[237px] 2xl:w-[284px] h-[247px] 2xl:h-[297px]">
       <div className="flex mt-[38.3%] mx-auto border-[3px] border-primary-regular rounded-full w-[26px] 2xl:w-[32px] h-[26px] 2xl:h-[32px]">
-        <div className="m-auto w-[60%] h-[15%] bg-thirdly-regular before:block before:rotate-90 before:w-[100%] before:h-[100%] before:content-[''] before:bg-dash" />
+        <div className="m-auto w-[60%] h-[15%] bg-thirdly-regular before:block before:rotate-90 before:w-[100%] before:h-[100%] before:content-[''] before:bg-thirdly-regular" />
       </div>
       <p className="mx-auto mt-[6.3%] text-[16px] 2xl:text-[19px] text-secondary-regular text-center">
         メニューを追加
