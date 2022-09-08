d745d38d117e56425e6e8317b3b9fc02b356624d
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-09-08 23:03:47 +0900

    add: OrderCardの大枠を作成

--- components/OrderCard.tsx
+++ components/OrderCard.tsx
@@ -1,3 +1,5 @@
 export const OrderCard = (): JSX.Element => {
-  return <div></div>;
+  return (
+    <div className="rounded-[10px] md:rounded-[12px] lg:rounded-[15px] xl:rounded-[19px] 2xl:rounded-[23px] w-[419px] md:w-[503px] lg:w-[671px] xl:w-[838px] 2xl:w-[1006px] h-[131px] md:h-[157px] lg:h-[210px] xl:h-[262px] 2xl:h-[315px] shadow-2xl bg-primary-regular"></div>
+  );
 };
