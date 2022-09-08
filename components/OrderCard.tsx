58d4f3ab1e0d58cdbdccc81f15c7164b3a0aec1b
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-09-08 23:49:23 +0900

    add: 注文番号の追加

--- components/OrderCard.tsx
+++ components/OrderCard.tsx
@@ -1,5 +1,11 @@
 export const OrderCard = (): JSX.Element => {
   return (
-    <div className="rounded-[10px] md:rounded-[12px] lg:rounded-[15px] xl:rounded-[19px] 2xl:rounded-[23px] w-[419px] md:w-[503px] lg:w-[671px] xl:w-[838px] 2xl:w-[1006px] h-[131px] md:h-[157px] lg:h-[210px] xl:h-[262px] 2xl:h-[315px] shadow-2xl bg-primary-regular"></div>
+    <div className="overflow-hidden rounded-[10px] md:rounded-[12px] lg:rounded-[15px] xl:rounded-[19px] 2xl:rounded-[23px] w-[419px] md:w-[503px] lg:w-[671px] xl:w-[838px] 2xl:w-[1006px] h-[131px] md:h-[157px] lg:h-[210px] xl:h-[262px] 2xl:h-[315px] shadow-2xl bg-primary-regular">
+      <div className="text-primary-regular">
+        <div className=" mt-[2.7%] ml-[2.7%] text-[10px] md:text-[12px] lg:text-[15px] xl:text-[19px] 2xl:text-[23px] font-bold font-menu-card leading-[1.2]">
+          注文番号 #9521
+        </div>
+      </div>
+    </div>
   );
 };
