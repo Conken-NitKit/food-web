b237298bd034ee6fae34a35f1ca5a2d8bd67b920
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-09-09 23:44:36 +0900

    add: 合計の追加

--- components/OrderCard.tsx
+++ components/OrderCard.tsx
@@ -1,11 +1,11 @@
 export const OrderCard = (): JSX.Element => {
   return (
-    <div className="overflow-hidden rounded-[10px] md:rounded-[12px] lg:rounded-[15px] xl:rounded-[19px] 2xl:rounded-[23px] w-[419px] md:w-[503px] lg:w-[671px] xl:w-[838px] 2xl:w-[1006px] h-[131px] md:h-[157px] lg:h-[210px] xl:h-[262px] 2xl:h-[315px] shadow-2xl bg-primary-regular">
+    <div className="mb-[56px] overflow-hidden w-[1048px] h-[328px] rounded-[24px]  shadow-2xl bg-primary-regular">
       <div className="text-primary-regular">
-        <div className=" mt-[2.7%] ml-[2.7%] text-[10px] md:text-[12px] lg:text-[15px] xl:text-[19px] 2xl:text-[23px] font-bold font-menu-card leading-[1.2]">
+        <div className="mt-[2.7%] ml-[2.7%] text-[10px] md:text-[12px] lg:text-[15px] xl:text-[19px] 2xl:text-[23px] font-bold font-menu-card leading-[1.2]">
           注文番号 #9521
         </div>
-        <div className="flex mx-auto mt-[3.5%] w-[92%]">
+        <div className="mx-auto mt-[3.5%] w-[92%]">
           <ul className="my-auto font-bold font-menu-card pb-[1.7%] w-full border-b border-primary-regular border-dashed">
             <li className="flex items-center justify-center mb-[1.7%] w-full">
               <div className="flex items-center justify-center w-[5.8%] aspect-square text-[11px] md:text-[13px] lg:text-[18px] xl:text-[22px] 2xl:text-[27px] rounded-[16px] bg-accent-secondary-light-regular">
@@ -36,6 +36,9 @@ export const OrderCard = (): JSX.Element => {
               </div>
             </li>
           </ul>
+          <div className="">
+          <div className="mt-[2.2%] font-bold font-menu-card text-[28px]"><span className="text-[18px]">合計 ￥</span>4,260</div>
+        </div>
         </div>
       </div>
     </div>
