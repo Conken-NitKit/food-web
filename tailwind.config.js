14a50dcb894a1d001a3ed4c166672935b71fe016
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-09-10 00:32:36 +0900

    rename: order-card-acceptをorder-card-completeにリネーム

--- tailwind.config.js
+++ tailwind.config.js
@@ -176,7 +176,7 @@ module.exports = {
         "accent-secondary-regular": theme("colors.goldenyellow.a100"),
         "accent-secondary-light-regular": theme("colors.navajowhite.a100"),
         "order-card-reject": theme("colors.pink.a10"),
-        "order-card-accept": theme("colors.green.a10"),
+        "order-card-complete": theme("colors.green.a10"),
       }),
       textColor: (theme) => ({
         "primary-inverted": theme("colors.white.a100"),
