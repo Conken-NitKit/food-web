fc0eb839ccc7808cdc86ae718a3a68e4abc58dc3
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-09-11 17:56:12 +0900

    style: スタイルの修正

--- components/OrderCard.tsx
+++ components/OrderCard.tsx
@@ -7,10 +7,10 @@ interface Props {
 export const OrderCard = ({ state }: Props): JSX.Element => {
   return (
     <div className="overflow-hidden mb-[17px] md:mb-[20px] lg:mb-[27px] xl:mb-[34px] 2xl:mb-[40px] rounded-[10px] md:rounded-[12px] lg:rounded-[15px] xl:rounded-[19px] 2xl:rounded-[23px] w-[419px] md:w-[503px] lg:w-[671px] xl:w-[838px] 2xl:w-[1006px] h-[131px] md:h-[157px] lg:h-[210px] xl:h-[262px] 2xl:h-[315px] font-bold font-menu-card text-primary-regular shadow-2xl bg-primary-regular">
-      <div className="mt-[2.7%] ml-[2.7%] text-[10px] md:text-[12px] lg:text-[15px] xl:text-[19px] 2xl:text-[23px] leading-[1.2]">
+      <div className="pt-[2.7%] ml-[2.7%] text-[10px] md:text-[12px] lg:text-[15px] xl:text-[19px] 2xl:text-[23px] leading-[1.2]">
         注文番号 #9521
       </div>
-      <div className="mx-auto mt-[3.5%] border-b border-primary-regular border-dashed w-[95%] h-[44%]">
+      <div className="mx-auto mt-[3.5%] border-b border-primary-regular border-dashed w-[95%] h-[44.2%]">
         <ul className="overflow-scroll mx-auto pb-[1.7%] w-[97%] h-full">
           <li className="flex items-center mb-[1.7%]">
             <div className="flex items-center justify-center rounded-[6px] md:rounded-[8px] lg:rounded-[10px] xl:rounded-[13px] 2xl:rounded-[15px] aspect-square w-[5.8%] text-[11px] md:text-[13px] lg:text-[18px] xl:text-[22px] 2xl:text-[27px] bg-accent-secondary-light-regular">
@@ -56,7 +56,7 @@ export const OrderCard = ({ state }: Props): JSX.Element => {
           4,260
         </div>
         {state === "wait" && (
-          <div className="flex justify-between ml-auto mt-[1.5%] w-[13%]">
+          <div className="flex justify-between ml-auto mt-[1.5%] w-[14.1%]">
             <div className="border border-reject-regular aspect-square w-[41.2%] rounded-[6px] md:rounded-[8px] lg:rounded-[10px] xl:rounded-[13px] 2xl:rounded-[15px] bg-order-card-reject"></div>
             <div className="border border-complete-regular aspect-square w-[41.2%] rounded-[6px] md:rounded-[8px] lg:rounded-[10px] xl:rounded-[13px] 2xl:rounded-[15px] bg-order-card-complete"></div>
           </div>
