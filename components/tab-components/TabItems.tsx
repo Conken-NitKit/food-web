3c21588cf5c0593d25dfae61f906c4478c39a58f
平田海輝 <umiteru2004@gmail.com>
2022-09-12 21:20:16 +0900

    fix: タブメニューの、カテゴリ選択時の下線の色が適用されない不具合を修正しました。

--- components/tab-components/TabItems.tsx
+++ components/tab-components/TabItems.tsx
@@ -14,14 +14,16 @@ export const TabItems = ({ categoryName, isSelected }: Props): JSX.Element => {
       <p
         className={classNames(
           "flex justify-center items-center px-[5px] md:px-[6px] lg:px-2 xl:px-[10px] 2xl:px-3 h-[92%] text-[10px] md:text-xs lg:text-[15px] xl:text-[19px] 2xl:text-23[px]",
-          isSelected ? "text-primary-regular font-bold" : "text-secondary-regular"
+          isSelected
+            ? "text-primary-regular font-bold"
+            : "text-secondary-regular"
         )}
       >
         {categoryName}
       </p>
 
       {isSelected && (
-        <hr className="border-0 rounded-[1px] lg:rounded-sm h-[8%] bg-gradient-to-b bg-accent-gradient-primary" />
+        <hr className="border-0 rounded-[1px] lg:rounded-sm h-[8%] bg-accent-gradient-primary" />
       )}
     </div>
   );
