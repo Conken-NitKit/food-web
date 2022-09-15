864682efb65779c20d639bfd6b5699bb201848ec
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-09-15 17:06:49 +0900

    update: 合計金額の計算処理を切り分け

--- components/OrderCard.tsx
+++ components/OrderCard.tsx
@@ -8,6 +8,13 @@ interface Props {
   };
 }
 
+const getTotalAmount: (
+  previousValue: number,
+  currentValue: OrderDetail
+) => number = (previousValue, currentValue) => {
+  return previousValue + currentValue.price * currentValue.orderVolume;
+};
+
 export const OrderCard = ({ content }: Props): JSX.Element => {
   return (
     <div className="overflow-hidden mb-[17px] md:mb-[20px] lg:mb-[27px] xl:mb-[34px] 2xl:mb-[40px] rounded-[10px] md:rounded-[12px] lg:rounded-[15px] xl:rounded-[19px] 2xl:rounded-[23px] w-[419px] md:w-[503px] lg:w-[671px] xl:w-[838px] 2xl:w-[1006px] h-[131px] md:h-[157px] lg:h-[210px] xl:h-[262px] 2xl:h-[315px] text-[10px] md:text-[12px] lg:text-[15px] xl:text-[19px] 2xl:text-[23px] font-bold font-menu-card text-primary-regular shadow-regular bg-primary-regular">
@@ -42,9 +49,7 @@ export const OrderCard = ({ content }: Props): JSX.Element => {
         <p className="text-[7px] md:text-[9px] lg:text-[12px] xl:text-[14px] 2xl:text-[17px]">
           合計 ￥
           <span className="text-[11px] md:text-[13px] lg:text-[18px] xl:text-[22px] 2xl:text-[27px]">
-            {content.products
-              .reduce((acc, cur) => acc + cur.price * cur.orderVolume, 0)
-              .toLocaleString()}
+            {content.products.reduce(getTotalAmount, 0).toLocaleString()}
           </span>
         </p>
         {content.state === "waiting" && (
