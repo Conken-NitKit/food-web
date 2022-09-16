70cd11edbb277d67b167628a0550b9614668c20d
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-09-16 22:47:47 +0900

    add: イメージを追加

--- components/HomeHead.tsx
+++ components/HomeHead.tsx
@@ -1,3 +1,9 @@
 export const HomeHead = (): JSX.Element => {
-  return <div className="mx-auto mt-[72px] w-[666px] h-[129px]"></div>;
+  return (
+    <div className="mx-auto mt-[72px] w-[666px] h-[129px]">
+      <div className="flex items-center justify-center rounded-[16px] h-full aspect-square text-[48px] bg-secondary-regular ">
+        🖥
+      </div>
+    </div>
+  );
 };
