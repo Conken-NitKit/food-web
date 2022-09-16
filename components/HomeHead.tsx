945adcf1aa1764a7ee8c9f7ead85d1e5449d7258
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-09-16 22:49:01 +0900

    add: 店名を追加

--- components/HomeHead.tsx
+++ components/HomeHead.tsx
@@ -1,8 +1,11 @@
 export const HomeHead = (): JSX.Element => {
   return (
-    <div className="mx-auto mt-[72px] w-[666px] h-[129px]">
+    <div className="flex mx-auto mt-[72px] w-[666px] h-[129px]">
       <div className="flex items-center justify-center rounded-[16px] h-full aspect-square text-[48px] bg-secondary-regular ">
         🖥
+      </div>
+      <div>
+        <h1 className="text-[36px] font-bold leading-[42px]">コンピュータ研究部</h1>
       </div>
     </div>
   );
