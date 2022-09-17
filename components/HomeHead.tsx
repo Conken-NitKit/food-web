b6511d62cff2c556c23874697c3451f40172c96d
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-09-17 18:38:36 +0900

    add: コピーボタンを追加

--- components/HomeHead.tsx
+++ components/HomeHead.tsx
@@ -1,3 +1,10 @@
+const urlCopy = () => {
+  const url = "コピー";
+  navigator.clipboard.writeText(url).then(() => {
+    alert("copied!");
+  });
+};
+
 export const HomeHead = (): JSX.Element => {
   return (
     <div className="flex mx-auto mt-[72px] w-[666px] h-[129px]">
@@ -11,14 +18,14 @@ export const HomeHead = (): JSX.Element => {
         <p className="mt-[1.49%] text-[16px] leading-[18.75px]">
           https://food.netlify/conken-festival
         </p>
-        <div>
-        <a className="flex items-center justify-center mt-[3.35%] border border-secondary-regular rounded-[14px] w-[77.2%] h-[21.7%] text-[14px] font-bold">
-          モバイルオーダー用のページを確認する
-        </a>
+        <div className="flex mt-[3.35%] h-[21.7%]">
+          <a className="flex items-center justify-center border border-secondary-regular rounded-[14px] w-[77.2%] h-full text-[14px] font-bold">
+            モバイルオーダー用のページを確認する
+          </a>
+          <button onClick={urlCopy} className="ml-[6.81%] border border-secondary-regular rounded-[14px] w-[16.39%] h-full font-bold text-primary-inverted bg-primary-dark-regular">コピー</button>
         </div>
       </div>
     </div>
   );
 };
 //display, position, top/bottom/right/left, m, border, p, w, h
-
