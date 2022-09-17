3069088d3434ec97eab6e8d558276840bf354663
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-09-17 21:13:08 +0900

    style: レスポンシブ対応

--- components/HomeHead.tsx
+++ components/HomeHead.tsx
@@ -7,24 +7,24 @@ const urlCopy = () => {
 
 export const HomeHead = (): JSX.Element => {
   return (
-    <div className="flex mx-auto mt-[72px] w-[666px] h-[129px]">
-      <div className="flex items-center justify-center rounded-[16px] h-full aspect-square text-[48px] bg-secondary-regular ">
+    <div className="flex mx-auto mt-[29px] md:mt-[35px] lg:mt-[46px] xl:mt-[58px] 2xl:mt-[69px] w-[266px] md:w-[320px] lg:w-[426px] xl:w-[533px] 2xl:w-[639px] h-[52px] md:h-[62px] lg:h-[83px] xl:h-[103px] 2xl:h-[124px]">
+      <div className="flex items-center justify-center rounded-[6px] md:rounded-[8px] lg:rounded-[10px] xl:rounded-[13px] 2xl:rounded-[15px] h-full aspect-square text-[19px] md:text-[23px] lg:text-[31px] xl:text-[38px] 2xl:text-[46px] bg-secondary-regular">
         🖥
       </div>
       <div className="w-full ml-[3.3%]">
-        <h1 className="text-[36px] font-bold text-primary-regular leading-[42px]">
+        <h1 className="mt-[1%] text-[14px] md:text-[17px] lg:text-[23px] xl:text-[29px] 2xl:text-[35px] font-bold text-primary-regular leading-[1.17]">
           コンピュータ研究部
         </h1>
-        <p className="mt-[1.56%] text-[16px] text-secondary-regular leading-[18.75px]">
+        <p className="mt-[1.56%] text-[10px] xl:text-[13px] 2xl:text-[15px] text-secondary-regular leading-[1.19]">
           https://food.netlify/conken-festival
         </p>
         <div className="flex mt-[3.5%] h-[21.7%]">
-          <a className="flex items-center justify-center border border-secondary-regular rounded-[14px] w-[80.4%] h-full text-[14px] text-primary-regular font-bold">
+          <a className="flex items-center justify-center border border-secondary-regular rounded-[6px] md:rounded-[7px] lg:rounded-[9px] xl:rounded-[11px] 2xl:rounded-[13px] w-[80.4%] h-full text-[11px] 2xl:text-[13px] text-primary-regular font-bold">
             <p>モバイルオーダー用のページを確認する</p>
           </a>
           <button
             onClick={urlCopy}
-            className="ml-auto border border-secondary-regular rounded-[14px] w-[17.1%] h-full font-bold text-primary-inverted bg-primary-dark-regular"
+            className="ml-auto border border-secondary-regular rounded-[6px] md:rounded-[7px] lg:rounded-[9px] xl:rounded-[11px] 2xl:rounded-[13px] w-[17.1%] h-full text-[11px] 2xl:text-[13px] font-bold text-primary-inverted bg-primary-dark-regular"
           >
             <p>コピー</p>
           </button>
@@ -33,4 +33,3 @@ export const HomeHead = (): JSX.Element => {
     </div>
   );
 };
-//display, position, top/bottom/right/left, m, border, p, w, h
