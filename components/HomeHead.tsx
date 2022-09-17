ffeefe1246c8feae1b92efc509c3388e23120c7c
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-09-17 18:55:34 +0900

    style: スタイルの微調整

--- components/HomeHead.tsx
+++ components/HomeHead.tsx
@@ -11,18 +11,23 @@ export const HomeHead = (): JSX.Element => {
       <div className="flex items-center justify-center rounded-[16px] h-full aspect-square text-[48px] bg-secondary-regular ">
         🖥
       </div>
-      <div className="w-full ">
-        <h1 className="text-[36px] font-bold leading-[42px]">
+      <div className="w-full ml-[3.3%]">
+        <h1 className="text-[36px] font-bold text-primary-regular leading-[42px]">
           コンピュータ研究部
         </h1>
-        <p className="mt-[1.49%] text-[16px] leading-[18.75px]">
+        <p className="mt-[1.56%] text-[16px] text-secondary-regular leading-[18.75px]">
           https://food.netlify/conken-festival
         </p>
-        <div className="flex mt-[3.35%] h-[21.7%]">
-          <a className="flex items-center justify-center border border-secondary-regular rounded-[14px] w-[77.2%] h-full text-[14px] font-bold">
+        <div className="flex mt-[3.5%] h-[21.7%]">
+          <a className="flex items-center justify-center border border-secondary-regular rounded-[14px] w-[80.4%] h-full text-[14px] text-primary-regular font-bold">
             モバイルオーダー用のページを確認する
           </a>
-          <button onClick={urlCopy} className="ml-[6.81%] border border-secondary-regular rounded-[14px] w-[16.39%] h-full font-bold text-primary-inverted bg-primary-dark-regular">コピー</button>
+          <button
+            onClick={urlCopy}
+            className="ml-auto border border-secondary-regular rounded-[14px] w-[17.1%] h-full font-bold text-primary-inverted bg-primary-dark-regular"
+          >
+            コピー
+          </button>
         </div>
       </div>
     </div>
