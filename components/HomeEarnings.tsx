71217c702bfd5906a45e8f11d9e7cfad54887fd1
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-09-18 18:11:07 +0900

    add: 日付を追加

--- components/HomeEarnings.tsx
+++ components/HomeEarnings.tsx
@@ -1,6 +1,12 @@
 export const HomeEarnigs = (): JSX.Element => {
-  return(
-<div className="mt-[50px] ml-[267px] w-[564.4px] h-[234px]">
-</div>
+  return (
+    <div className="mt-[50px] ml-[267px] w-[564.4px] h-[234px]">
+      <div className="flex items-center  h-[14.1%]">
+        <h1 className="text-[28px] font-bold">今月</h1>
+        <p className="ml-[2.12%] text-[16px]">
+          {new Date().getFullYear()}年{new Date().getMonth() + 1}月
+        </p>
+      </div>
+    </div>
   );
 };
