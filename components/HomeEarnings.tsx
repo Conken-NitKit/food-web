2512392e8ec2f7e8ba03514b3bcefe68657ee59e
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-09-18 23:10:19 +0900

    style: スタイル調整

--- components/HomeEarnings.tsx
+++ components/HomeEarnings.tsx
@@ -1,6 +1,6 @@
 export const HomeEarnigs = (): JSX.Element => {
   return (
-    <div className="mt-[20px] md:mt-[24px] lg:mt-[32px] xl:mt-[40px] 2xl:mt-[48px] mx-auto 2xl:ml-[256px] w-[226px] md:w-[271px] lg:w-[361px] xl:w-[452px] 2xl:w-[542px] h-[94px] md:h-[112px] lg:h-[150px] xl:h-[187px] 2xl:h-[225px] text-primary-regular">
+    <div className="mt-[20px] md:mt-[24px] lg:mt-[32px] xl:mt-[40px] 2xl:mt-[48px] mx-auto w-[226px] md:w-[271px] lg:w-[361px] xl:w-[452px] 2xl:w-[542px] h-[94px] md:h-[112px] lg:h-[150px] xl:h-[187px] 2xl:h-[225px] text-primary-regular">
       <div className="flex items-center h-[14.1%]">
         <h1 className="text-[11px] md:text-[13px] lg:text-[18px] xl:text-[22px] 2xl:text-[27px] font-bold">今月</h1>
         <p className="ml-[2.12%] text-[10px] xl:text-[13px] 2xl:text-[15px]">
@@ -19,7 +19,7 @@ export const HomeEarnigs = (): JSX.Element => {
           <p className="text-[13px] md:text-[15px] lg:text-[20px] xl:text-[26px] 2xl:text-[31px] font-bold leading-none">962</p>
         </div>
       </div>
-      <a className="flex items-center justify-center ml-auto mt-[8.3%] border border-secondary-regular rounded-[6px] md:rounded-[7px] lg:rounded-[9px] xl:rounded-[11px] 2xl:rounded-[13px] w-[73.7%] h-[12.1%] text-primary-regular font-bold">
+      <a className="flex items-center justify-center ml-auto mt-[8.3%] border border-secondary-regular rounded-[6px] md:rounded-[7px] lg:rounded-[9px] xl:rounded-[11px] 2xl:rounded-[13px] w-[73.7%] h-[12.1%] text-[11px] 2xl:text-[13px] text-primary-regular font-bold">
         <p>売上解析ページを確認する</p>
       </a>
     </div>
