7ca00a0083f3d23f7d4d0f3a5222cc46c23e2925
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-09-18 23:08:17 +0900

    style: レスポンシブ対応

--- components/HomeEarnings.tsx
+++ components/HomeEarnings.tsx
@@ -1,25 +1,25 @@
 export const HomeEarnigs = (): JSX.Element => {
   return (
-    <div className="mt-[50px] ml-[267px] w-[564.4px] h-[234px] text-primary-regular">
-      <div className="flex items-center  h-[14.1%]">
-        <h1 className="text-[28px] font-bold">今月</h1>
-        <p className="ml-[2.12%] text-[16px]">
+    <div className="mt-[20px] md:mt-[24px] lg:mt-[32px] xl:mt-[40px] 2xl:mt-[48px] mx-auto 2xl:ml-[256px] w-[226px] md:w-[271px] lg:w-[361px] xl:w-[452px] 2xl:w-[542px] h-[94px] md:h-[112px] lg:h-[150px] xl:h-[187px] 2xl:h-[225px] text-primary-regular">
+      <div className="flex items-center h-[14.1%]">
+        <h1 className="text-[11px] md:text-[13px] lg:text-[18px] xl:text-[22px] 2xl:text-[27px] font-bold">今月</h1>
+        <p className="ml-[2.12%] text-[10px] xl:text-[13px] 2xl:text-[15px]">
           {new Date().getFullYear()}年{new Date().getMonth() + 1}月
         </p>
       </div>
-      <div className="flex items-center mt-[30px] h-[41%]">
-        <div className="pr-[12.6%] border-r text-center">
-          <p className="mb-[10.5%] text-[16px]">売上合計</p>
-          <p className="text-[24px] font-bold">
-            ¥ <span className="text-[32px]">361,280</span>
+      <div className="flex items-center mt-[5.3%] w-full h-[41%]">
+        <div className="pr-[12.6%] border-r border-secondary-regular text-center">
+          <p className="mb-[13.5%] text-[10px] xl:text-[13px] 2xl:text-[15px]">売上合計</p>
+          <p className="text-[10px] md:text-[12px] lg:text-[15px] xl:text-[19px] 2xl:text-[23px] font-bold leading-none">
+            ¥ <span className="text-[13px] md:text-[15px] lg:text-[20px] xl:text-[26px] 2xl:text-[31px]">361,280</span>
           </p>
         </div>
         <div className="pl-[14.2%] text-center">
-          <p className="mb-[10.5%] text-[16px]">注文回数合計</p>
-          <p className="text-[32px] font-bold">962</p>
+          <p className="mb-[20.9%] text-[10px] xl:text-[13px] 2xl:text-[15px]">注文回数合計</p>
+          <p className="text-[13px] md:text-[15px] lg:text-[20px] xl:text-[26px] 2xl:text-[31px] font-bold leading-none">962</p>
         </div>
       </div>
-      <a className="flex items-center justify-center ml-auto mt-[8.3%] border border-secondary-regular rounded-[14px] w-[73.7%] h-[12.1%] text-primary-regular font-bold">
+      <a className="flex items-center justify-center ml-auto mt-[8.3%] border border-secondary-regular rounded-[6px] md:rounded-[7px] lg:rounded-[9px] xl:rounded-[11px] 2xl:rounded-[13px] w-[73.7%] h-[12.1%] text-primary-regular font-bold">
         <p>売上解析ページを確認する</p>
       </a>
     </div>
