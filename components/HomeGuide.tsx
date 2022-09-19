25c605463ceb0a5fa44c7aa0eafe8b23d3079f6b
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-09-19 15:42:41 +0900

    update: タグを適切なものに変更

--- components/HomeGuide.tsx
+++ components/HomeGuide.tsx
@@ -1,7 +1,9 @@
 export const HomeGuide = (): JSX.Element => {
   return (
     <div className="mx-auto mt-[14px] md:mt-[17px] lg:mt-[23px] xl:mt-[29px] 2xl:mt-[35px] w-[260px] md:w-[312px] lg:w-[415px] xl:w-[519px] 2xl:w-[623px] h-[121px] md:h-[145px] lg:h-[193px] xl:h-[242px] 2xl:h-[290px] text-primary-regular">
-      <h1 className="text-[11px] md:text-[13px] lg:text-[18px] xl:text-[22px] 2xl:text-[27px] font-bold">使い方</h1>
+      <div className="text-[11px] md:text-[13px] lg:text-[18px] xl:text-[22px] 2xl:text-[27px] font-bold">
+        使い方
+      </div>
       <ul className="overflow-scroll flex flex-wrap mt-[4.93%] h-[85%]">
         <li className="flex mb-[3.3%] w-[47.45%] h-[25%] even:ml-auto">
           <div className="flex items-center justify-center rounded-[6px] md:rounded-[8px] lg:rounded-[10px] xl:rounded-[13px] 2xl:rounded-[15px] aspect-square h-full bg-secondary-regular text-[24px]">
