ab931c1d527b24e362445d3ec4dec9b5df41b662
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-09-19 17:03:28 +0900

    update: 使い方の文が長い時の表示を修正

--- components/HomeGuide.tsx
+++ components/HomeGuide.tsx
@@ -1,66 +1,31 @@
-export const HomeGuide = (): JSX.Element => {
+interface Props {
+  guide: {
+    id: number;
+    ideogram: string;
+    description: string;
+  }[];
+}
+
+export const HomeGuide = ({ guide }: Props): JSX.Element => {
   return (
     <div className="mx-auto mt-[14px] md:mt-[17px] lg:mt-[23px] xl:mt-[29px] 2xl:mt-[35px] w-[260px] md:w-[312px] lg:w-[415px] xl:w-[519px] 2xl:w-[623px] h-[121px] md:h-[145px] lg:h-[193px] xl:h-[242px] 2xl:h-[290px] text-primary-regular">
       <div className="text-[11px] md:text-[13px] lg:text-[18px] xl:text-[22px] 2xl:text-[27px] font-bold">
         使い方
       </div>
       <ul className="overflow-scroll flex flex-wrap mt-[4.93%] h-[85%]">
-        <li className="flex mb-[3.3%] w-[47.45%] h-[25%] even:ml-auto">
-          <div className="flex items-center justify-center rounded-[6px] md:rounded-[8px] lg:rounded-[10px] xl:rounded-[13px] 2xl:rounded-[15px] aspect-square h-full bg-secondary-regular text-[24px]">
-            🖥
-          </div>
-          <p className="overflow-scroll ml-[6.5%] text-[6px] md:text-[8px] lg:text-[10px] xl:text-[13px] 2xl:text-[15px] font-bold">
-            ああああああああああああああああああああああああああ
-          </p>
-        </li>
-        <li className="flex mb-[3.3%] w-[47.45%] h-[25%] even:ml-auto">
-          <div className="flex items-center justify-center rounded-[6px] md:rounded-[8px] lg:rounded-[10px] xl:rounded-[13px] 2xl:rounded-[15px] aspect-square h-full bg-secondary-regular text-[24px]">
-            🖥
-          </div>
-          <p className="overflow-scroll ml-[6.5%] text-[6px] md:text-[8px] lg:text-[10px] xl:text-[13px] 2xl:text-[15px] font-bold">
-            あああああああああああああああああああああああああああ
-          </p>
-        </li>
-        <li className="flex mb-[3.3%] w-[47.45%] h-[25%] even:ml-auto">
-          <div className="flex items-center justify-center rounded-[6px] md:rounded-[8px] lg:rounded-[10px] xl:rounded-[13px] 2xl:rounded-[15px] aspect-square h-full bg-secondary-regular text-[24px]">
-            🖥
-          </div>
-          <p className="overflow-scroll ml-[6.5%] text-[6px] md:text-[8px] lg:text-[10px] xl:text-[13px] 2xl:text-[15px] font-bold">
-            あああああああああああああああああああああああああああ
-          </p>
-        </li>
-        <li className="flex mb-[3.3%] w-[47.45%] h-[25%] even:ml-auto">
-          <div className="flex items-center justify-center rounded-[6px] md:rounded-[8px] lg:rounded-[10px] xl:rounded-[13px] 2xl:rounded-[15px] aspect-square h-full bg-secondary-regular text-[24px]">
-            🖥
-          </div>
-          <p className="overflow-scroll ml-[6.5%] text-[6px] md:text-[8px] lg:text-[10px] xl:text-[13px] 2xl:text-[15px] font-bold">
-            あああああああああああああああああああああああああああ
-          </p>
-        </li>
-        <li className="flex mb-[3.3%] w-[47.45%] h-[25%] even:ml-auto">
-          <div className="flex items-center justify-center rounded-[6px] md:rounded-[8px] lg:rounded-[10px] xl:rounded-[13px] 2xl:rounded-[15px] aspect-square h-full bg-secondary-regular text-[24px]">
-            🖥
-          </div>
-          <p className="overflow-scroll ml-[6.5%] text-[6px] md:text-[8px] lg:text-[10px] xl:text-[13px] 2xl:text-[15px] font-bold">
-            あああああああああああああああああああああああああああ
-          </p>
-        </li>
-        <li className="flex mb-[3.3%] w-[47.45%] h-[25%] even:ml-auto">
-          <div className="flex items-center justify-center rounded-[6px] md:rounded-[8px] lg:rounded-[10px] xl:rounded-[13px] 2xl:rounded-[15px] aspect-square h-full bg-secondary-regular text-[24px]">
-            🖥
-          </div>
-          <p className="overflow-scroll ml-[6.5%] text-[6px] md:text-[8px] lg:text-[10px] xl:text-[13px] 2xl:text-[15px] font-bold">
-            あああああああああああああああああああああああああああ
-          </p>
-        </li>
-        <li className="flex mb-[3.3%] w-[47.45%] h-[25%] even:ml-auto">
-          <div className="flex items-center justify-center rounded-[6px] md:rounded-[8px] lg:rounded-[10px] xl:rounded-[13px] 2xl:rounded-[15px] aspect-square h-full bg-secondary-regular text-[24px]">
-            🖥
-          </div>
-          <p className="overflow-scroll ml-[6.5%] text-[6px] md:text-[8px] lg:text-[10px] xl:text-[13px] 2xl:text-[15px] font-bold">
-            あああああああああああああああああああああああああああ
-          </p>
-        </li>
+        {Object.values(guide).map((guide) => (
+          <li
+            key={guide.id}
+            className="flex mb-[3.3%] w-[47.45%] h-[25%] even:ml-auto"
+          >
+            <div className="flex items-center justify-center rounded-[6px] md:rounded-[8px] lg:rounded-[10px] xl:rounded-[13px] 2xl:rounded-[15px] aspect-square h-full bg-secondary-regular text-[24px]">
+              {guide.ideogram}
+            </div>
+            <p className="line-clamp-2 ml-[6.5%] h-[80%] text-[6px] md:text-[8px] lg:text-[10px] xl:text-[13px] 2xl:text-[15px] font-bold">
+              {guide.description}
+            </p>
+          </li>
+        ))}
       </ul>
     </div>
   );
