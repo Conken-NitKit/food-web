ced5d5d39630827545458ca791ebf110d816f410
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-09-19 15:16:34 +0900

    add: 使い方のリストを追加

--- components/HomeGuide.tsx
+++ components/HomeGuide.tsx
@@ -1,5 +1,65 @@
 export const HomeGuide = (): JSX.Element => {
   return (
-    <div className="mx-auto mt-[36px] w-[649px] h-[302px]"></div>
+    <div className="mx-auto mt-[36px] w-[649px] h-[302px] text-primary-regular">
+      <h1 className="text-[28px] font-bold">使い方</h1>
+      <ul className="overflow-scroll flex flex-wrap mt-[4.93%] h-[85%]">
+        <li className="flex mb-[3.3%] w-[47.45%] h-[64px] even:ml-auto">
+          <div className="flex items-center justify-center rounded-[16px] aspect-square h-full bg-secondary-regular text-[24px]">
+            🖥
+          </div>
+          <p className="overflow-scroll ml-[6.5%] text-[16px] font-bold">
+            ああああああああああああああああああああああああああ
+          </p>
+        </li>
+        <li className="flex mb-[3.3%] w-[47.45%] h-[64px] even:ml-auto">
+          <div className="flex items-center justify-center rounded-[16px] aspect-square h-full bg-secondary-regular text-[24px]">
+            🖥
+          </div>
+          <p className="overflow-scroll ml-[6.5%] text-[16px] font-bold">
+            あああああああああああああああああああああああああああ
+          </p>
+        </li>
+        <li className="flex mb-[3.3%] w-[47.45%] h-[64px] even:ml-auto">
+          <div className="flex items-center justify-center rounded-[16px] aspect-square h-full bg-secondary-regular text-[24px]">
+            🖥
+          </div>
+          <p className="overflow-scroll ml-[6.5%] text-[16px] font-bold">
+            あああああああああああああああああああああああああああ
+          </p>
+        </li>
+        <li className="flex mb-[3.3%] w-[47.45%] h-[64px] even:ml-auto">
+          <div className="flex items-center justify-center rounded-[16px] aspect-square h-full bg-secondary-regular text-[24px]">
+            🖥
+          </div>
+          <p className="overflow-scroll ml-[6.5%] text-[16px] font-bold">
+            あああああああああああああああああああああああああああ
+          </p>
+        </li>
+        <li className="flex mb-[3.3%] w-[47.45%] h-[64px] even:ml-auto">
+          <div className="flex items-center justify-center rounded-[16px] aspect-square h-full bg-secondary-regular text-[24px]">
+            🖥
+          </div>
+          <p className="overflow-scroll ml-[6.5%] text-[16px] font-bold">
+            あああああああああああああああああああああああああああ
+          </p>
+        </li>
+        <li className="flex mb-[3.3%] w-[47.45%] h-[64px] even:ml-auto">
+          <div className="flex items-center justify-center rounded-[16px] aspect-square h-full bg-secondary-regular text-[24px]">
+            🖥
+          </div>
+          <p className="overflow-scroll ml-[6.5%] text-[16px] font-bold">
+            あああああああああああああああああああああああああああ
+          </p>
+        </li>
+        <li className="flex mb-[3.3%] w-[47.45%] h-[64px] even:ml-auto">
+          <div className="flex items-center justify-center rounded-[16px] aspect-square h-full bg-secondary-regular text-[24px]">
+            🖥
+          </div>
+          <p className="overflow-scroll ml-[6.5%] text-[16px] font-bold">
+            あああああああああああああああああああああああああああ
+          </p>
+        </li>
+      </ul>
+    </div>
   );
 };
