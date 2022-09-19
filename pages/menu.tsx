8b9f297ab301bf1269390785f5783b17aaca5efe
saezuri510 <saezuru510@gmail.com>
2022-09-19 13:50:47 +0900

    update: 要素の間隔を子html要素から親html要素で指定するように変更しました。

--- pages/menu.tsx
+++ pages/menu.tsx
@@ -6,68 +6,71 @@ import { MenuContent } from "../types/MenuContent";
 
 //仮置きのデータ（実際はデータベースから取得する）
 const MenuContents: {
-  [key: string]: {
-    product: MenuContent;
-    isSold: boolean;
-  };
+    [key: string]: {
+        product: MenuContent;
+        isSold: boolean;
+    };
 } = {
-  hamburger1: {
-    product: {
-      ideogram: "🍔",
-      name: "ビーフバーガー１",
-      promotion:
-        "おいしさも食べごたえもビッグな人気メニュー、ビッグマック。パティが倍でそのビッグな食べごたえもボリュームアップ。",
-      price: 490000,
+    hamburger1: {
+        product: {
+            ideogram: "🍔",
+            name: "ビーフバーガー１",
+            promotion:
+                "おいしさも食べごたえもビッグな人気メニュー、ビッグマック。パティが倍でそのビッグな食べごたえもボリュームアップ。",
+            price: 490000,
+        },
+        isSold: false,
     },
-    isSold: false,
-  },
-  hamburger2: {
-    product: {
-      ideogram: "🍔",
-      name: "ビーフバーガー２",
-      promotion:
-        "おいしさも食べごたえもビッグな人気メニュー、ビッグマック。パティが倍でそのビッグな食べごたえもボリュームアップ。",
-      price: 490000,
+    hamburger2: {
+        product: {
+            ideogram: "🍔",
+            name: "ビーフバーガー２",
+            promotion:
+                "おいしさも食べごたえもビッグな人気メニュー、ビッグマック。パティが倍でそのビッグな食べごたえもボリュームアップ。",
+            price: 490000,
+        },
+        isSold: false,
     },
-    isSold: false,
-  },
-  hamburger3: {
-    product: {
-      ideogram: "🍔",
-      name: "ビーフバーガー３",
-      promotion:
-        "おいしさも食べごたえもビッグな人気メニュー、ビッグマック。パティが倍でそのビッグな食べごたえもボリュームアップ。",
-      price: 490000,
+    hamburger3: {
+        product: {
+            ideogram: "🍔",
+            name: "ビーフバーガー３",
+            promotion:
+                "おいしさも食べごたえもビッグな人気メニュー、ビッグマック。パティが倍でそのビッグな食べごたえもボリュームアップ。",
+            price: 490000,
+        },
+        isSold: true,
     },
-    isSold: true,
-  },
-  hamburger4: {
-    product: {
-      ideogram: "🍔",
-      name: "ビーフバーガー４",
-      promotion:
-        "おいしさも食べごたえもビッグな人気メニュー、ビッグマック。パティが倍でそのビッグな食べごたえもボリュームアップ。",
-      price: 490000,
+    hamburger4: {
+        product: {
+            ideogram: "🍔",
+            name: "ビーフバーガー４",
+            promotion:
+                "おいしさも食べごたえもビッグな人気メニュー、ビッグマック。パティが倍でそのビッグな食べごたえもボリュームアップ。",
+            price: 490000,
+        },
+        isSold: false,
     },
-    isSold: false,
-  },
 };
 
 const Menu: NextPage = () => {
-  return (
-    <FeatureLayout type="menu">
-      <ul className="flex flex-wrap ml-[-38px] 2xl:ml-[-46px]">
-        {Object.values(MenuContents).map((content) => (
-          <li key={content.product.name}>
-            <MenuCard content={content.product} isSold={content.isSold} />
-          </li>
-        ))}
-        <li key="add-menu-card">
-          <AddMenuCard />
-        </li>
-      </ul>
-    </FeatureLayout>
-  );
+    return (
+        <FeatureLayout type="menu">
+            <ul className="flex flex-wrap ml-[-38px] 2xl:ml-[-46px] gap-y-[48px] 2xl:gap-y-[58px] gap-x-[38px] 2xl:gap-x-[46px]">
+                {Object.values(MenuContents).map((content) => (
+                    <li key={content.product.name}>
+                        <MenuCard
+                            content={content.product}
+                            isSold={content.isSold}
+                        />
+                    </li>
+                ))}
+                <li key="add-menu-card">
+                    <AddMenuCard />
+                </li>
+            </ul>
+        </FeatureLayout>
+    );
 };
 
 export default Menu;
