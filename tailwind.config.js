6ba09e86739a005f696e44345e20f20114e0a6b1
saezuri510 <saezuru510@gmail.com>
2022-09-20 22:16:34 +0900

    update: boxShadowをレスポンシブ対応しました。

--- tailwind.config.js
+++ tailwind.config.js
@@ -196,7 +196,8 @@ module.exports = {
         "menu-card": "lato",
       },
       boxShadow: {
-        "regular": "0px 2px 8px 0px rgba(0, 0, 0, 0.25)",
+        regular: "0px 2px 8px 0px rgba(0, 0, 0, 0.25)",
+        large: "0px 3px 12px 0px rgba(0, 0, 0, 0.25)",
       },
       lineHeight: {
         basic: "1.17",
