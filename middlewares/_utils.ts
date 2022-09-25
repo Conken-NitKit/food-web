1b0d0c6eda70e409ff01282452e37a5b3a065489
kubo-hide-kun <hide44041@gmail.com>
2022-09-25 17:24:58 +0900

    docs: update JSDoc of withTransitObserver :books:

--- middlewares/_utils.ts
+++ middlewares/_utils.ts
@@ -5,7 +5,15 @@ export type TransitObserverOption = {
   shouldConsole?: boolean;
 };
 
-// NOTE: middlewareを通過した際に、どのmiddlewareが通過したかをconsole.logで出力する
+/**
+ * middlewareを通過した際に、どのmiddlewareが通過したかをconsole.logで出力する
+ * @param middleware 監視対象のmiddleware
+ * @param middlewareName middlewareの名前
+ * @param option
+ * @param option.locale ロケール
+ * @param option.shouldConsole console.logを出力するかどうか
+ * @returns 通過時にconsole.logを出力するmiddleware
+ */
 export const withTransitObserver = (
   middleware: ComposableMiddleware,
   middlewareName: string,
