e06b495cec50b51d519e900fd01d23467571cd66
kubo-hide-kun <hide44041@gmail.com>
2022-09-27 19:08:09 +0900

    feat: appply context to next-auth api :sparkles:

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,30 +1,37 @@
 import NextAuth from "next-auth";
 import Auth0Provider from "next-auth/providers/auth0";
-import { secretConfig } from "../../../lib/core/secretConfig";
+import { context } from "../../../lib/context";
+import { Logger } from "../../../types/logger";
+
+export abstract class Context {
+  abstract logger: Logger;
+  abstract secretConfig: {
+    auth0: {
+      clientId: string;
+      clientSecret: string;
+      issuer?: string;
+    };
+    nextAuth: {
+      url?: string;
+      secret?: string;
+    };
+  };
+}
 
 /**
  * @description
  * 認証関連のAPIを生成するファクトリ関数です
  */
-const generateNextAuth = (config: {
-  auth0: {
-    clientId: string;
-    clientSecret: string;
-    issuer?: string;
-  };
-  nextAuth: {
-    secret?: string;
-  };
-}) => {
+const generateNextAuth = (context: Context) => {
   return NextAuth({
     providers: [
       Auth0Provider({
-        clientId: config.auth0.clientId,
-        clientSecret: config.auth0.clientSecret,
-        issuer: config.auth0.issuer,
+        clientId: context.secretConfig.auth0.clientId,
+        clientSecret: context.secretConfig.auth0.clientSecret,
+        issuer: context.secretConfig.auth0.issuer,
       }),
     ],
-    secret: config.nextAuth.secret,
+    secret: context.secretConfig.nextAuth.secret,
     callbacks: {
       async signIn({ user, account, profile, email, credentials }) {
         console.log({ user, account, profile, email, credentials });
@@ -42,28 +49,7 @@ const generateNextAuth = (config: {
         return session;
       },
     },
-    debug: true,
-    logger: {
-      error(code, metadata) {
-        console.error(code, metadata);
-      },
-      warn(code) {
-        console.warn(code);
-      },
-      debug(code, metadata) {
-        console.debug(code, metadata);
-      },
-    },
   });
 };
 
-export default generateNextAuth({
-  auth0: {
-    clientId: secretConfig.auth0.clientId,
-    clientSecret: secretConfig.auth0.clientSecret,
-    issuer: secretConfig.auth0.issuer,
-  },
-  nextAuth: {
-    secret: secretConfig.nextAuth.secret,
-  },
-});
+export default generateNextAuth(context);
