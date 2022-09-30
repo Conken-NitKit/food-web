f3d4afccec8cff4822df93721678136b50d154e2
saezuri510 <saezuru510@gmail.com>
2022-09-30 17:59:07 +0900

    add: MenuModalのModal内のテキストのフォントと大きさを設定しました。

--- components/MenuModal.tsx
+++ components/MenuModal.tsx
@@ -17,13 +17,13 @@ export const MenuModal = () => {
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={() => setModalIsOpen(false)}
-        className="absolute top-[29px] left-[53px] pt-[15px] pb-[17px] pr-[20px] pl-[19px] rounded-[8px] w-[472px] h-[565px] bg-white-a100"
+        className="absolute top-[29px] left-[53px] pt-[15px] pb-[17px] pr-[20px] pl-[19px] rounded-[8px] w-[472px] h-[565px] bg-white-a100 font-menu-card"
       >
-        <h3 className="h-[17px]">メニューを新規作成</h3>
+        <h3 className="h-[17px] font-bold text-[15px]">メニューを新規作成</h3>
         <div className="grid grid-cols-2 mt-[14px] gap-x-[19px]">
-          <div className="space-y-[12px]">
+          <div className="space-y-[12px] text-[12px]">
             <div>
-              <label htmlFor="menu_name" className="block mb-[3px]">
+              <label htmlFor="menu_name" className="block mb-[3px] font-bold">
                 メニュー名
               </label>
               <input
@@ -34,7 +34,7 @@ export const MenuModal = () => {
               />
             </div>
             <div>
-              <label htmlFor="menu_price" className="block mb-[6px]">
+              <label htmlFor="menu_price" className="block mb-[6px] font-bold">
                 価格
               </label>
               <input
@@ -45,14 +45,17 @@ export const MenuModal = () => {
               />
             </div>
             <div>
-              <label className="block mb-[4px]">状態</label>
+              <label className="block mb-[4px] font-bold">状態</label>
               <select>
                 <option>販売中</option>
                 <option>売り切れ</option>
               </select>
             </div>
             <div>
-              <label htmlFor="menu_description" className="block mb-[4px]">
+              <label
+                htmlFor="menu_description"
+                className="block mb-[4px] font-bold"
+              >
                 説明文
               </label>
               <textarea
@@ -63,10 +66,10 @@ export const MenuModal = () => {
             </div>
           </div>
           <div>
-            <p className="mb-[5px]">商品イメージ</p>
+            <p className="mb-[5px] font-bold text-[12px]">商品イメージ</p>
             <div className="mb-[8px]">🍔</div>
             <div>
-              <label>絵文字を選択する</label>
+              <label className="text-[10px]">絵文字を選択する</label>
               <select>
                 <option>🍔</option>
                 <option>🍟</option>
@@ -75,7 +78,7 @@ export const MenuModal = () => {
             </div>
           </div>
         </div>
-        <div className="flex items-end justify-end">
+        <div className="flex items-end justify-end text-[10px]">
           <button onClick={CreateMenu} className="mr-[9px]">
             商品を追加
           </button>
