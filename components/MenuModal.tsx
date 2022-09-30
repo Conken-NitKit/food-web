68bab04e2823bc86845ed1c86d6d33190799a948
saezuri510 <saezuru510@gmail.com>
2022-09-30 17:03:51 +0900

    add: MenuModalのModal内の要素の位置を設定しました。

--- components/MenuModal.tsx
+++ components/MenuModal.tsx
@@ -13,61 +13,72 @@ export const MenuModal = () => {
   return (
     <>
       <button onClick={() => setModalIsOpen(true)}>modal</button>
+      {/* className="absolute top-[294px] left-[532px] rounded-[8px] w-[472px] h-[565px] bg-white-a100" */}
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={() => setModalIsOpen(false)}
-        className="absolute top-[294px] left-[532px] rounded-[8px] w-[472px] h-[565px] bg-white-a100"
+        className="absolute top-[29px] left-[53px] pt-[15px] pb-[17px] pr-[20px] pl-[19px] rounded-[8px] w-[472px] h-[565px] bg-white-a100"
       >
-        <div>
-          <h3>メニューを新規作成</h3>
-          <div>
-            <input
-              type="text"
-              value={menuName}
-              onChange={(e) => setMenuName(e.target.value)}
-              id="menu_name"
-            />
-            <label htmlFor="menu_name">メニュー名</label>
+        <h3 className="h-[17px]">メニューを新規作成</h3>
+        <div className="grid grid-cols-2 mt-[14px] gap-x-[19px]">
+          <div className="space-y-[12px]">
+            <div>
+              <label htmlFor="menu_name" className="block mb-[3px]">
+                メニュー名
+              </label>
+              <input
+                type="text"
+                value={menuName}
+                onChange={(e) => setMenuName(e.target.value)}
+                id="menu_name"
+              />
+            </div>
+            <div>
+              <label htmlFor="menu_price" className="block mb-[6px]">
+                価格
+              </label>
+              <input
+                type="number"
+                value={menuPrice}
+                onChange={(e) => setMenuPrice(e.target.value)}
+                id="menu_price"
+              />
+            </div>
+            <div>
+              <label className="block mb-[4px]">状態</label>
+              <select>
+                <option>販売中</option>
+                <option>売り切れ</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="menu_description" className="block mb-[4px]">
+                説明文
+              </label>
+              <textarea
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                id="menu_description"
+              />
+            </div>
           </div>
           <div>
-            <input
-              type="number"
-              value={menuPrice}
-              onChange={(e) => setMenuPrice(e.target.value)}
-              id="menu_price"
-            />
-            <label htmlFor="menu_price">価格</label>
-          </div>
-          <div>
-            <label>状態</label>
-            <select>
-              <option>販売中</option>
-              <option>売り切れ</option>
-            </select>
-          </div>
-          <div>
-            <textarea
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              id="menu_description"
-            />
-            <label htmlFor="menu_description">説明文</label>
+            <p className="mb-[5px]">商品イメージ</p>
+            <div className="mb-[8px]">🍔</div>
+            <div>
+              <label>絵文字を選択する</label>
+              <select>
+                <option>🍔</option>
+                <option>🍟</option>
+                <option>🍿</option>
+              </select>
+            </div>
           </div>
         </div>
-        <div>
-          <p>商品イメージ</p>
-          <div>🍔</div>
-          <div>
-            <label>絵文字を選択する</label>
-            <select>
-              <option>🍔</option>
-              <option>🍟</option>
-              <option>🍿</option>
-            </select>
-          </div>
-        </div>
-        <div>
-          <button onClick={CreateMenu}>商品を追加</button>
+        <div className="flex items-end justify-end">
+          <button onClick={CreateMenu} className="mr-[9px]">
+            商品を追加
+          </button>
           <button onClick={CreateMenu}>商品を追加</button>
         </div>
       </Modal>
