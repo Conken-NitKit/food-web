78d3767c591757d7bc28256c85c429f73e9db123
平田海輝 <umiteru2004@gmail.com>
2022-10-14 21:06:51 +0900

    update: メニューアイテムにパスを追加しました。

--- constants/menusItems.ts
+++ constants/menusItems.ts
@@ -2,40 +2,49 @@ import { PageId } from "../types";
 
 interface MenuItems {
   id: PageId;
+  path: string;
   name: string;
 }
 
 export const MENUS_ITEMS: Record<PageId, MenuItems> = {
   home: {
     id: "home",
+    path: "/",
     name: "ホーム",
   },
   menu: {
     id: "menu",
+    path: "menu",
     name: "メニュー",
   },
   orders: {
     id: "orders",
+    path: "orders",
     name: "注文状況",
   },
   sales: {
     id: "sales",
+    path: "sales",
     name: "売上解析",
   },
   shift: {
     id: "shift",
+    path: "shift",
     name: "シフト",
   },
   users: {
     id: "users",
+    path: "users",
     name: "ユーザー",
   },
   monitoring: {
     id: "monitoring",
+    path: "monitoring",
     name: "モニタリング",
   },
   settings: {
     id: "settings",
+    path: "settings",
     name: "設定",
   },
 };
