3e5cbb6578b2f9e8d1d59d8db29f1250d6063857
平田海輝 <umiteru2004@gmail.com>
2022-10-25 08:13:11 +0900

    ユーザー管理ページのレイアウトコンポーネントをFeatureLayout → ManagementPageLayoutに変更しました。

--- pages/owner/[storeId]/users.tsx
+++ pages/owner/[storeId]/users.tsx
@@ -1,12 +1,9 @@
 import { NextPage } from "next";
 import { FeatureLayout } from "../../../components/layouts";
+import { ManagementPageLayout } from "../../../components/layouts/ManagementPage";
 
 const Users: NextPage = () => {
-  return (
-    <FeatureLayout type="users">
-      <></>
-    </FeatureLayout>
-  );
+  return <ManagementPageLayout type="users"></ManagementPageLayout>;
 };
 
 export default Users;
