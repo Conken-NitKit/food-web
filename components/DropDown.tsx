f18868bcb624ff6bb400b9ac8e2e28b2df8fe3a5
kiseki-kiseki <kcanggua@gmail.com>
2022-10-24 17:57:12 +0900

    fix:absolute追加して位置固定させた

--- components/DropDown.tsx
+++ components/DropDown.tsx
@@ -32,20 +32,22 @@ const DropDown = (props: Props): JSX.Element => {
             {selectedItem}
           </div>
           {dropDownOpen ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="ionicon"
-              viewBox="0 0 512 512"
-            >
-              <path
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="32"
-                d="M368 368L144 144M368 144L144 368"
-              />
-            </svg>
+            <div className="absolute">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="ionicon"
+                viewBox="0 0 512 512"
+              >
+                <path
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="32"
+                  d="M368 368L144 144M368 144L144 368"
+                />
+              </svg>
+            </div>
           ) : (
             <div
               onClick={() => {
