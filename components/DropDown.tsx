f8b5d43bb78f1cf4fcefbe5b5b86165c87cd2289
kiseki-kiseki <kcanggua@gmail.com>
2022-10-25 23:13:50 +0900

    fix:svg表示

--- components/DropDown.tsx
+++ components/DropDown.tsx
@@ -32,9 +32,11 @@ const DropDown = (props: Props): JSX.Element => {
             {selectedItem}
           </div>
           {isDropDownOpen ? (
-            <div className="absolute">
+            <div>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
                 className="ionicon"
                 viewBox="0 0 512 512"
               >
