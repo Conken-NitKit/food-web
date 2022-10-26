79013e24349c0d4ffe9e7853b25637a7c8cf2485
平田海輝 <umiteru2004@gmail.com>
2022-10-27 08:29:00 +0900

    update: ManagementPageLayoutのTipsにスタイルを追加しました。

--- components/layouts/ManagementPage.tsx
+++ components/layouts/ManagementPage.tsx
@@ -9,7 +9,10 @@ interface Props {
 export const ManagementPageLayout = ({ type, tips }: Props): JSX.Element => {
   return (
     <FeatureLayout type={type}>
-      <p>{tips}</p>
+      {/* note: FeatureLayoutのページタイトルの下のmarginが必要以上に大きかったので、マイナスのmarginを指定しています。 */}
+      <p className="mt-[-10px] md:mt-[-12px] lg:mt-[-16px] xl:mt-[-20px] 2xl:mt-[-24px] text-primary-regular text-[10px] xl:text-xs 2xl:text-base">
+        {tips}
+      </p>
     </FeatureLayout>
   );
 };
