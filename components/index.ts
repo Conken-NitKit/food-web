191aeba3df085bef0f5ad37e861208573d8468a1
kiseki-kiseki <kcanggua@gmail.com>
2022-10-27 17:13:17 +0900

    fix:使ってないimport削除

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,5 @@
 export { MenuItemIcon } from "./MenuItemIcon";
 export { MenuCard } from "./MenuCard";
 export { AddMenuCard } from "./AddMenuCard";
-import OperationCard from "./OperationCard";
 export { Badge } from "./Badge";
 export { OrderCard } from "./OrderCard";
