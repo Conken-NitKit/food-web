f7372647161c3f2fb0baaab9b6e7e58855e45363
平田海輝 <umiteru2004@gmail.com>
2022-10-30 07:50:12 +0900

    update: ユーザーカードの境界線を追加しました。

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -1,3 +1,5 @@
+import classNames from "classnames";
+
 type type = "user-card" | "add-card";
 
 interface Props {
@@ -5,7 +7,14 @@ interface Props {
 }
 
 export const UserCard = ({ type }: Props): JSX.Element => {
+  const isAddCard = type === "add-card";
+
   return (
-    <div className="w-full h-9 md:h-11 lg:h-14 xl:h-[70px] 2xl:h-[84px]"></div>
+    <div
+      className={classNames(
+        "w-full h-9 md:h-11 lg:h-14 xl:h-[70px] 2xl:h-[84px]",
+        { "border-t border-secondary-regular": !isAddCard }
+      )}
+    ></div>
   );
 };
