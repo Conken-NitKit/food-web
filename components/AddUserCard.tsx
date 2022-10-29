caa62444cdb0f8d051dd5878cb1dde0556b14921
平田海輝 <umiteru2004@gmail.com>
2022-10-30 08:27:13 +0900

    update: ユーザーを追加するボタンを追加しました。

--- components/AddUserCard.tsx
+++ components/AddUserCard.tsx
@@ -1,5 +1,7 @@
 export const AddUserCard = (): JSX.Element => {
   return (
-    <div className="w-full h-9 md:h-11 lg:h-14 xl:h-[70px] 2xl:h-[84px]"></div>
+    <div className="w-full h-9 md:h-11 lg:h-14 xl:h-[70px] 2xl:h-[84px]">
+      <button>+</button>
+    </div>
   );
 };
