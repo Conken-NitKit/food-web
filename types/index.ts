d1382eeb35e9ded3d41b18772baeb6969f0df0a2
平田海輝 <umiteru2004@gmail.com>
2022-10-30 07:32:42 +0900

    update: ユーザーデータの型をtypesからexportしました。

--- types/index.ts
+++ types/index.ts
@@ -5,3 +5,4 @@ export type { LayoutProps } from "./LayoutProps";
 export type { IconProps } from "./IconProps";
 export type { OrderState } from "./OrderState";
 export type { OrderDetail } from "./OrderDetail";
+export type { User } from "./User";
