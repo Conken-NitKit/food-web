16d7e3e72ce9c889b8ab5c62dd4c52d2eb340338
平田海輝 <umiteru2004@gmail.com>
2022-10-29 12:37:11 +0900

    update: ユーザー管理ページにユーザーカードを追加しました。

--- pages/owner/[storeId]/users.tsx
+++ pages/owner/[storeId]/users.tsx
@@ -1,12 +1,16 @@
 import { NextPage } from "next";
+
 import { ManagementPageLayout } from "../../../components/layouts/ManagementPage";
+import { UserCard } from "../../../components";
 
 const Users: NextPage = () => {
   const tips = "システムの共同編集者を管理することができます。";
 
   return (
     <ManagementPageLayout type="users" tips={tips}>
-      <div className="mt-2.5 md:mt-3 lg:mt-3.5 xl:mt-5 2xl:mt-6 ml-1.5 md:ml-2 lg:ml-2.5 xl:ml-3 2xl:ml-4 mr-4 md:mr-5 lg:mr-7 xl:mr-9 2xl:mr-11"></div>
+      <div className="mt-2.5 md:mt-3 lg:mt-3.5 xl:mt-5 2xl:mt-6 ml-1.5 md:ml-2 lg:ml-2.5 xl:ml-3 2xl:ml-4 mr-4 md:mr-5 lg:mr-7 xl:mr-9 2xl:mr-11">
+        <UserCard type="add-card" />
+      </div>
     </ManagementPageLayout>
   );
 };
