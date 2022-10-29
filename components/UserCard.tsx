2dbb8f8423ac7dbcb45f00a29e843263a8bf2141
平田海輝 <umiteru2004@gmail.com>
2022-10-29 12:32:12 +0900

    update: ユーザーカードにカードタイプ属性を追加しました。

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -1,4 +1,10 @@
-export const UserCard = (): JSX.Element => {
+type type = "user-card" | "add-card";
+
+interface Props {
+  type: type;
+}
+
+export const UserCard = ({ type }: Props): JSX.Element => {
   return (
     <div className="w-full h-9 md:h-11 lg:h-14 xl:h-[70px] 2xl:h-[84px]"></div>
   );
