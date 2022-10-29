6dd511d88bb68d4b5b73eea0f56f4b8262fc7736
平田海輝 <umiteru2004@gmail.com>
2022-10-30 07:21:31 +0900

    update: ユーザー管理ページにユーザーカードを追加しました。

--- pages/owner/[storeId]/users.tsx
+++ pages/owner/[storeId]/users.tsx
@@ -16,6 +16,9 @@ const Users: NextPage = () => {
     <ManagementPageLayout type="users" tips={tips}>
       <div className="mt-2.5 md:mt-3 lg:mt-3.5 xl:mt-5 2xl:mt-6 ml-1.5 md:ml-2 lg:ml-2.5 xl:ml-3 2xl:ml-4 mr-4 md:mr-5 lg:mr-7 xl:mr-9 2xl:mr-11">
         <UserCard type="add-card" />
+        {USERS_MOCK.map((user) => (
+          <UserCard type="user-card" />
+        ))}
       </div>
     </ManagementPageLayout>
   );
