9ba49f071739f0c902f54fe224cf5bf6044ac5bd
平田海輝 <umiteru2004@gmail.com>
2022-10-31 12:10:21 +0900

    update: 「共同編集者の追加」カードに共同編集者を追加するボタンを追加しました。

--- components/AddUserCard.tsx
+++ components/AddUserCard.tsx
@@ -1,7 +1,9 @@
 export const AddUserCard = (): JSX.Element => {
   return (
-    <div className="w-full h-9 md:h-11 lg:h-14 xl:h-[70px] 2xl:h-[84px]">
-      <button>+</button>
+    <div className="flex items-center w-full h-9 md:h-11 lg:h-14 xl:h-[70px] 2xl:h-[84px]">
+      <button className="border border-secondary-regular rounded-md md:rounded-lg lg:rounded-lg xl:rounded-xl 2xl:rounded-2xl w-6 md:w-[29px] lg:w-[37px] xl:w-[47px] 2xl:w-14 h-2/3 font-bold text-base md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl leading-none">
+        ＋
+      </button>
     </div>
   );
 };
