437cd265349a4c6ed761fdbcb8d3853b133d967a
平田海輝 <umiteru2004@gmail.com>
2022-10-31 12:12:37 +0900

    update: 「共同編集者の追加」カードにテキストを追加しました。

--- components/AddUserCard.tsx
+++ components/AddUserCard.tsx
@@ -4,6 +4,8 @@ export const AddUserCard = (): JSX.Element => {
       <button className="border border-secondary-regular rounded-md md:rounded-lg lg:rounded-lg xl:rounded-xl 2xl:rounded-2xl w-6 md:w-[29px] lg:w-[37px] xl:w-[47px] 2xl:w-14 h-2/3 font-bold text-base md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl leading-none">
         ＋
       </button>
+
+      <p>共同編集者の追加</p>
     </div>
   );
 };
