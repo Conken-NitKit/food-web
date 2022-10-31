efd933d01c9d3cc202dad95b7b0deeb7abec5063
平田海輝 <umiteru2004@gmail.com>
2022-11-01 08:25:49 +0900

    update: ユーザーカードのタイトルスペースのコンポーネントをcomponentsからexportしました。

--- components/index.ts
+++ components/index.ts
@@ -4,3 +4,4 @@ export { AddMenuCard } from "./AddMenuCard";
 export { OrderCard } from "./OrderCard";
 export { AddUserCard } from "./AddUserCard";
 export { UserCard } from "./UserCard";
+export { UserCardTitleSpace } from "./UserCardTitleSpace";
