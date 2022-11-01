8ab4d964aadbfba88416dcaeb5c361317cc4725d
平田海輝 <umiteru2004@gmail.com>
2022-11-01 17:02:38 +0900

    update: ユーザーカードの大枠にスタイルを追加しました。

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -1,3 +1,5 @@
 export const UserCard = (): JSX.Element => {
-  return <></>;
+  return (
+    <div className="flex items-center border-t border-secondary-regular w-full h-9 md:h-11 lg:h-14 xl:h-[70px] 2xl:h-[84px]"></div>
+  );
 };
