1b48a00753ee4987ced1f0c47d14234007bad0a3
平田海輝 <umiteru2004@gmail.com>
2022-11-01 17:38:28 +0900

    update: ユーザーカードタイトルのコンポーネントにスタイルを追加しました。

---
+++ components/UserCardTitle.tsx
@@ -0,0 +1,22 @@
+import classNames from "classnames";
+
+import { UserCardItemProps } from "../types";
+
+interface Props extends UserCardItemProps {
+  title: string;
+}
+
+export const UserCardTitle = ({ type, title }: Props): JSX.Element => {
+  const isAddCardItem = type === "add-card";
+
+  return (
+    <p
+      className={classNames(
+        "text-primary-regular text-xs lg:text-base xl:text-xl 2xl:text-2xl",
+        { "font-bold": !isAddCardItem }
+      )}
+    >
+      {title}
+    </p>
+  );
+};
