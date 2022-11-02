a5d812b32ac3affb64ab5d73b658ddc00a94999a
平田海輝 <umiteru2004@gmail.com>
2022-11-02 16:29:05 +0900

    update: ユーザーカードにユーザーカードのタイトルスペースのコンポーネントを追加しました。

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -1,9 +1,11 @@
-import { UserCardOutline } from ".";
+import { UserCardOutline, UserCardTitleSpace } from ".";
 
 export const UserCard = (): JSX.Element => {
   return (
     <UserCardOutline cardType="user-card">
-      <></>
+      <UserCardTitleSpace>
+        <></>
+      </UserCardTitleSpace>
     </UserCardOutline>
   );
 };
