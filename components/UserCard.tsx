996868d675cd63c9e31b1c572d28aa172b84bcc8
平田海輝 <umiteru2004@gmail.com>
2022-11-02 16:37:53 +0900

    update: ユーザーカードにタイトルを追加しました。

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -6,10 +6,14 @@ interface Props {
 }
 
 export const UserCard = ({ userData }: Props): JSX.Element => {
+  const userName = userData.name;
+  const isAdmin = userData.isAdmin;
+  const title: string = isAdmin ? `${userName}（管理者）` : userName;
+
   return (
     <UserCardOutline cardType="user-card">
       <UserCardTitleSpace>
-        <UserCardTitle cardType="user-card" title="" />
+        <UserCardTitle cardType="user-card" title={title} />
       </UserCardTitleSpace>
     </UserCardOutline>
   );
