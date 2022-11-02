6e4d096a87329471249d93ebc454036d4d417b38
平田海輝 <umiteru2004@gmail.com>
2022-11-02 16:46:55 +0900

    update: ユーザーカードにアイコンスペースを追加しました。

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -12,6 +12,8 @@ export const UserCard = ({ userData }: Props): JSX.Element => {
 
   return (
     <UserCardOutline cardType="user-card">
+      <div className="rounded-md md:rounded-lg lg:rounded-lg xl:rounded-xl 2xl:rounded-2xl w-6 md:w-[29px] lg:w-[37px] xl:w-[47px] 2xl:w-14 h-2/3"></div>
+
       <UserCardTitleSpace>
         <UserCardTitle cardType="user-card" title={title} />
       </UserCardTitleSpace>
