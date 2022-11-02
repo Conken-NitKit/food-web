15dcb835d96b50ff16966320a4452f180978b647
平田海輝 <umiteru2004@gmail.com>
2022-11-02 16:28:05 +0900

    uppdate: ユーザーカードにユーザーカードのアウトラインコンポーネントを追加しました。

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -1,5 +1,9 @@
+import { UserCardOutline } from ".";
+
 export const UserCard = (): JSX.Element => {
   return (
-    <div className="flex items-center border-t border-secondary-regular w-full h-9 md:h-11 lg:h-14 xl:h-[70px] 2xl:h-[84px]"></div>
+    <UserCardOutline cardType="user-card">
+      <></>
+    </UserCardOutline>
   );
 };
