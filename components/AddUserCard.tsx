f72fbee869c011c65030452f18a90d23e767cfef
平田海輝 <umiteru2004@gmail.com>
2022-11-02 16:26:20 +0900

    update: 「共同編集者の追加」カードにユーザーカードのアウトラインコンポーネントを追加しました。

--- components/AddUserCard.tsx
+++ components/AddUserCard.tsx
@@ -1,8 +1,8 @@
-import { UserCardTitleSpace, UserCardTitle } from ".";
+import { UserCardOutline, UserCardTitleSpace, UserCardTitle } from ".";
 
 export const AddUserCard = (): JSX.Element => {
   return (
-    <div className="flex items-center w-full h-9 md:h-11 lg:h-14 xl:h-[70px] 2xl:h-[84px]">
+    <UserCardOutline cardType="add-card">
       <button className="border border-secondary-regular rounded-md md:rounded-lg lg:rounded-lg xl:rounded-xl 2xl:rounded-2xl w-6 md:w-[29px] lg:w-[37px] xl:w-[47px] 2xl:w-14 h-2/3 font-bold text-primary-regular text-base md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl leading-none">
         ＋
       </button>
@@ -10,6 +10,6 @@ export const AddUserCard = (): JSX.Element => {
       <UserCardTitleSpace>
         <UserCardTitle cardType="add-card" title="共同編集者の追加" />
       </UserCardTitleSpace>
-    </div>
+    </UserCardOutline>
   );
 };
