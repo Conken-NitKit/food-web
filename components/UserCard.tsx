a3a6292a1c38cafaef0fb3c40e0b157f97a515c5
平田海輝 <umiteru2004@gmail.com>
2022-11-02 16:30:45 +0900

    update: ユーザーカードにユーザーカードタイトルのコンポーネントを追加しました。

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -1,10 +1,10 @@
-import { UserCardOutline, UserCardTitleSpace } from ".";
+import { UserCardOutline, UserCardTitleSpace, UserCardTitle } from ".";
 
 export const UserCard = (): JSX.Element => {
   return (
     <UserCardOutline cardType="user-card">
       <UserCardTitleSpace>
-        <></>
+        <UserCardTitle cardType="user-card" title="" />
       </UserCardTitleSpace>
     </UserCardOutline>
   );
