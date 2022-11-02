ce2741694adab5f5448e1ff322bc3efce04a4eaa
平田海輝 <umiteru2004@gmail.com>
2022-11-02 16:17:49 +0900

    update: ユーザーカードのアウトラインコンポーネントの大枠を追加しました。

--- components/UserCardOutline.tsx
+++ components/UserCardOutline.tsx
@@ -1,3 +1,18 @@
-export const UserCardOutline = (): JSX.Element => {
-  return <></>;
+import classNames from "classnames";
+
+import { UserCardItemProps } from "../types";
+
+export const UserCardOutline = ({
+  cardType,
+}: UserCardItemProps): JSX.Element => {
+  const isAddCardItem = cardType === "add-card";
+
+  return (
+    <div
+      className={classNames(
+        "flex items-center w-full h-9 md:h-11 lg:h-14 xl:h-[70px] 2xl:h-[84px]",
+        { "border-t border-secondary-regular": !isAddCardItem }
+      )}
+    ></div>
+  );
 };
