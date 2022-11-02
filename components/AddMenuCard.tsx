d4359c5b139e70635cfd543e2b34a23986524a23
saezuri510 <saezuru510@gmail.com>
2022-11-02 18:19:43 +0900

    update: formタグを使って商品を追加するようにしました。

--- components/AddMenuCard.tsx
+++ components/AddMenuCard.tsx
@@ -154,92 +154,98 @@ export const AddMenuCard = (): JSX.Element => {
         className="relative top-[50%] left-[50%] pt-[15px] pb-[17px] pr-[20px] pl-[19px] rounded-[8px] w-[472px] h-[565px] translate-x-[-50%] translate-y-[-50%] bg-white-a100 font-menu-card outline-none"
       >
         <h3 className="h-[17px] font-bold text-[15px]">メニューを新規作成</h3>
-        <div className="flex mt-[14px] gap-x-[19px]">
-          <div className="space-y-[12px] text-[12px]">
-            <div>
-              <label
-                htmlFor="menu_name"
-                className="block mb-[3px] font-bold w-[284px]"
-              >
-                メニュー名
-              </label>
-              <input
-                type="text"
-                value={NewMenu.product.name}
-                onChange={onChangeNewMenu}
-                id="menu_name"
-                className="border border-solid border-lightgray-a100 rounded box-border w-[284px] h-[31px]"
-                name="name"
-              />
+        <form onSubmit={createMenu}>
+          <div className="flex mt-[14px] gap-x-[19px]">
+            <div className="space-y-[12px] text-[12px]">
+              <div>
+                <label
+                  htmlFor="menu_name"
+                  className="block mb-[3px] font-bold w-[284px]"
+                >
+                  メニュー名
+                </label>
+                <input
+                  type="text"
+                  value={NewMenu.product.name}
+                  onChange={onChangeNewMenu}
+                  id="menu_name"
+                  className="border border-solid border-lightgray-a100 rounded box-border w-[284px] h-[31px]"
+                  name="name"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="menu_price"
+                  className="block mb-[6px] font-bold"
+                >
+                  価格
+                </label>
+                <input
+                  type="text"
+                  value={NewMenu.product.price}
+                  onChange={onChangeNewMenu}
+                  name="price"
+                  id="menu_price"
+                  className="border border-solid border-lightgray-a100 rounded box-border w-[284px] h-[31px]"
+                />
+              </div>
+              <div>
+                <label className="block mb-[4px] font-bold">状態</label>
+                <select
+                  className="border border-solid border-lightgray-a100 rounded box-border w-[284px] h-[31px] cursor-pointer"
+                  value={NewMenu.isSold ? "soldOut" : "onSale"}
+                  onChange={onChangeNewMenu}
+                  name="isSold"
+                >
+                  <option value="onSale">販売中</option>
+                  <option value="soldOut">売り切れ</option>
+                </select>
+              </div>
+              <div>
+                <label
+                  htmlFor="menu_description"
+                  className="block mb-[4px] font-bold"
+                >
+                  説明文
+                </label>
+                <textarea
+                  value={NewMenu.product.promotion}
+                  onChange={onChangeNewMenu}
+                  id="menu_description"
+                  className="border border-solid border-lightgray-a100 rounded box-border w-[284px] h-[215px]"
+                  name="promotion"
+                />
+              </div>
             </div>
             <div>
-              <label htmlFor="menu_price" className="block mb-[6px] font-bold">
-                価格
-              </label>
-              <input
-                type="text"
-                value={NewMenu.product.price}
-                onChange={onChangeNewMenu}
-                name="price"
-                id="menu_price"
-                className="border border-solid border-lightgray-a100 rounded box-border w-[284px] h-[31px]"
-              />
-            </div>
-            <div>
-              <label className="block mb-[4px] font-bold">状態</label>
-              <select
-                className="border border-solid border-lightgray-a100 rounded box-border w-[284px] h-[31px] cursor-pointer"
-                value={NewMenu.isSold ? "soldOut" : "onSale"}
-                onChange={onChangeNewMenu}
-                name="isSold"
+              <p className="mb-[5px] font-bold text-[12px]">商品イメージ</p>
+              <div className="flex mb-[8px] border border-solid border-lightgray-a100 rounded box-border w-[130px] h-[130px] items-center justify-center text-[46px] font-bold bg-accent-secondary-light-regular">
+                {IdeogramData.emoji}
+              </div>
+              <button
+                type="button"
+                className="rounded-[4px] w-[130px] h-[25px] text-[10px] bg-goldenyellow-a100 cursor-pointer"
+                onClick={openIdeogramModal}
               >
-                <option value="onSale">販売中</option>
-                <option value="soldOut">売り切れ</option>
-              </select>
-            </div>
-            <div>
-              <label
-                htmlFor="menu_description"
-                className="block mb-[4px] font-bold"
-              >
-                説明文
-              </label>
-              <textarea
-                value={NewMenu.product.promotion}
-                onChange={onChangeNewMenu}
-                id="menu_description"
-                className="border border-solid border-lightgray-a100 rounded box-border w-[284px] h-[215px]"
-                name="promotion"
-              />
+                絵文字を選択する
+              </button>
             </div>
           </div>
-          <div>
-            <p className="mb-[5px] font-bold text-[12px]">商品イメージ</p>
-            <div className="flex mb-[8px] border border-solid border-lightgray-a100 rounded box-border w-[130px] h-[130px] items-center justify-center text-[46px] font-bold bg-accent-secondary-light-regular">
-              {IdeogramData.emoji}
-            </div>
+          <div className="absolute bottom-[17px] right-[20px] text-[10px]">
             <button
-              className="rounded-[4px] w-[130px] h-[25px] text-[10px] bg-goldenyellow-a100 cursor-pointer"
-              onClick={openIdeogramModal}
+              type="button"
+              onClick={closeModal}
+              className="mr-[9px] border border-solid border-lightgray-a100 rounded box-border w-[67px] h-[23px]"
             >
-              絵文字を選択する
+              キャンセル
             </button>
+            <input
+              type="submit"
+              value="商品を追加"
+              className="rounded-[2px] w-[66px] h-[23px] bg-goldenyellow-a100 cursor-pointer"
+            />
           </div>
-        </div>
-        <div className="absolute bottom-[17px] right-[20px] text-[10px]">
-          <button
-            onClick={closeModal}
-            className="mr-[9px] border border-solid border-lightgray-a100 rounded box-border w-[67px] h-[23px]"
-          >
-            キャンセル
-          </button>
-          <button
-            onClick={createMenu}
-            className="rounded-[2px] w-[66px] h-[23px] bg-goldenyellow-a100"
-          >
-            商品を追加
-          </button>
-        </div>
+        </form>
       </Modal>
       <Modal
         isOpen={ideogramModalIsOpen}
