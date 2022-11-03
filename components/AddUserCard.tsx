2944548e9e24e83e4fcd42c35217dd397da4cb46
平田海輝 <umiteru2004@gmail.com>
2022-11-04 08:24:41 +0900

    update: 「共同編集者の追加」カードのユーザーを追加するボタンの+を中心に配置しました。

--- components/AddUserCard.tsx
+++ components/AddUserCard.tsx
@@ -3,7 +3,7 @@ import { UserCardOutline, UserCardTitleSpace, UserCardTitle } from ".";
 export const AddUserCard = (): JSX.Element => {
   return (
     <UserCardOutline cardType="add-card">
-      <button className="border border-secondary-regular rounded-md md:rounded-lg lg:rounded-lg xl:rounded-xl 2xl:rounded-2xl w-6 md:w-[29px] lg:w-[37px] xl:w-[47px] 2xl:w-14 h-2/3 font-bold text-primary-regular text-base md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl leading-none">
+      <button className="border border-secondary-regular rounded-md md:rounded-lg lg:rounded-lg xl:rounded-xl 2xl:rounded-2xl w-6 md:w-[29px] lg:w-[37px] xl:w-[47px] 2xl:w-14 h-2/3 font-bold text-primary-regular text-base md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl !leading-none">
         ＋
       </button>
 
