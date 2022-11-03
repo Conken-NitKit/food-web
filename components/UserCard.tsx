eb55748f0247ca4ad828ba2c2579e3291ac523dd
平田海輝 <umiteru2004@gmail.com>
2022-11-04 07:49:18 +0900

    update: ユーザーカードにアイコンを追加しました。

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 import { User } from "../types";
 import { UserCardOutline, UserCardTitleSpace, UserCardTitle } from ".";
 
@@ -6,13 +8,16 @@ interface Props {
 }
 
 export const UserCard = ({ userData }: Props): JSX.Element => {
+  const iconSrc = userData.icon;
   const userName = userData.name;
   const isAdmin = userData.isAdmin;
   const title: string = isAdmin ? `${userName}（管理者）` : userName;
 
   return (
     <UserCardOutline cardType="user-card">
-      <div className="rounded-md md:rounded-lg lg:rounded-lg xl:rounded-xl 2xl:rounded-2xl w-6 md:w-[29px] lg:w-[37px] xl:w-[47px] 2xl:w-14 h-2/3"></div>
+      <div className="relative rounded-md md:rounded-lg lg:rounded-lg xl:rounded-xl 2xl:rounded-2xl w-6 md:w-[29px] lg:w-[37px] xl:w-[47px] 2xl:w-14 h-2/3">
+        <Image src={iconSrc} layout="fill" />
+      </div>
 
       <UserCardTitleSpace>
         <UserCardTitle cardType="user-card" title={title} />
