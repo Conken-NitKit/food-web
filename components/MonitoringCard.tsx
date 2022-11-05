7b2b7c150b88677b0941c0755ae15bb52dcfd8f9
kiseki-kiseki <kcanggua@gmail.com>
2022-11-06 02:02:18 +0900

    remove:必要ない空白を消した

--- components/MonitoringCard.tsx
+++ components/MonitoringCard.tsx
@@ -14,19 +14,17 @@ const BADGE_COLORS: Record<MonitoringLog["type"], string> = {
 
 export const MonitoringCard = ({ log }: Props): JSX.Element => {
   return (
-    <div>
-      <div className="flex border  border-primary-regular rounded-[14px]  py-[10px] w-[1000px] h-[64px]">
-        <div className="rounded-full bg-thirdly-regular ml-[10px] w-[44px] h-[44px]"></div>
-        <div className="ml-[10px]">
-          <div className=" flex">
-            <div className="mt-[2px]">
-              <Badge bgcolor={BADGE_COLORS[log.type]}>{log.type}</Badge>
-            </div>
-            <div className="ml-[10px] text-xs font-bold">{log.message}</div>
+    <div className="flex border border-primary-regular rounded-[14px] py-[10px] w-[1000px] h-[64px]">
+      <div className="rounded-full bg-thirdly-regular ml-[10px] w-[44px] h-[44px]"></div>
+      <div className="ml-[10px]">
+        <div className=" flex">
+          <div className="mt-[2px]">
+            <Badge bgcolor={BADGE_COLORS[log.type]}>{log.type}</Badge>
           </div>
-          <div className="text-xs text-secondary-regular mt-[3px]">
-            {getFormattedDate(log.timestamp)}
-          </div>
+          <div className="ml-[10px] text-xs font-bold">{log.message}</div>
+        </div>
+        <div className="text-xs text-secondary-regular mt-[3px]">
+          {getFormattedDate(log.timestamp)}
         </div>
       </div>
     </div>
