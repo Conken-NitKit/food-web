4d358674491d16796588a05297e15b82d0fb401c
kiseki-kiseki <kcanggua@gmail.com>
2022-11-06 01:56:12 +0900

    remove:いらないdom削除

--- components/DropDown.tsx
+++ components/DropDown.tsx
@@ -44,34 +44,28 @@ const DropDown = ({ label, items, onChange }: Props): JSX.Element => {
   return (
     <div className="flex">
       <div className="opacity-[0.4] whitespace-nowrap">{label}</div>
-      <div className="ml-[10px]">
-        <div className="flex w-[100px]">
-          <div className="flex" onClick={onButtonClick}>
-            <div className="ml-[10px] whitespace-nowrap">
-              {selectedItem.label}
-            </div>
-            <div>{isOpen ? <CloseIcon /> : <ChevronDownIcon />}</div>
+      <div className="flex w-[100px] ml-[10px]">
+        <div className="flex" onClick={onButtonClick}>
+          <div className="ml-[10px] whitespace-nowrap">
+            {selectedItem.label}
           </div>
-          {isOpen && (
-            <div className="absolute mt-[30px] ml-[30px] py-[16px] z-10 rounded-2xl shadow-2xl bg-primary-regular">
-              <ul>
-                {Object.values(items).map((item) => (
-                  <li
-                    key={item.id}
-                    className="w-[120px] h-[30px] hover:bg-secondary-dark-regular"
-                  >
-                    <div
-                      onClick={createItemClickHandler(item)}
-                      className="ml-[20px]"
-                    >
-                      {item.label}
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
+          <div>{isOpen ? <CloseIcon /> : <ChevronDownIcon />}</div>
         </div>
+        {isOpen && (
+          <div className="absolute mt-[30px] ml-[30px] py-[16px] z-10 rounded-2xl shadow-2xl bg-primary-regular">
+            <ul>
+              {Object.values(items).map((item) => (
+                <li
+                  key={item.id}
+                  className="w-[120px] h-[30px] pl-[20px] hover:bg-secondary-dark-regular"
+                  onClick={createItemClickHandler(item)}
+                >
+                  {item.label}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
