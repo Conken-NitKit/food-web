b42fe4649c51ed86844af378e787d638ddaf0c3f
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-11-10 21:51:49 +0900

    update: LinkButtonをLinkタグでラップ

--- components/LinkButton.tsx
+++ components/LinkButton.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 interface Props {
   pcLabel: string;
   spLabel: string;
@@ -10,12 +12,11 @@ export const LinkButton = ({
   pagePath,
 }: Props): JSX.Element => {
   return (
-    <a
-      href={pagePath}
-      className="flex items-center justify-center border border-tertiary-regular rounded-[6px] md:rounded-[7px] lg:rounded-[9px] xl:rounded-[11px] 2xl:rounded-[13px] w-[166px] md:w-[199px] lg:w-[265px] xl:w-[332px] 2xl:w-[398px] h-[18px] xl:h-[22px] 2xl:h-[27px] text-[11px] 2xl:text-[13px] text-primary-regular"
-    >
-      <p className="hidden lg:block font-menu-card">{pcLabel}</p>
-      <p className="lg:hidden font-menu-card">{spLabel}</p>
-    </a>
+    <Link href={pagePath}>
+      <a className="flex items-center justify-center border border-tertiary-regular rounded-[6px] md:rounded-[7px] lg:rounded-[9px] xl:rounded-[11px] 2xl:rounded-[13px] w-[166px] md:w-[199px] lg:w-[265px] xl:w-[332px] 2xl:w-[398px] h-[18px] xl:h-[22px] 2xl:h-[27px] text-[11px] 2xl:text-[13px] text-primary-regular">
+        <p className="hidden lg:block font-menu-card">{pcLabel}</p>
+        <p className="lg:hidden font-menu-card">{spLabel}</p>
+      </a>
+    </Link>
   );
 };
