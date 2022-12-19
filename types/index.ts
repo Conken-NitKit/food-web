96f9764380f9c7e0273e1b1e409638433bcba622
平田海輝 <umiteru2004@gmail.com>
2022-12-19 14:05:52 +0900

    remove: types/index.tsからユーザーカードアイテムのpropsの型のexportを削除しました。

--- types/index.ts
+++ types/index.ts
@@ -3,7 +3,6 @@ export type { OrderCategoryId } from "./OrderCategoryId";
 
 export type { LayoutProps } from "./LayoutProps";
 export type { IconProps } from "./IconProps";
-export type { UserCardItemProps } from "./UserCardItemProps";
 
 export type { OrderState } from "./OrderState";
 export type { OrderDetail } from "./OrderDetail";
