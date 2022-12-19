51900112bfec4fd93bbe27ccebcda36096def89d
平田海輝 <umiteru2004@gmail.com>
2022-12-19 13:37:08 +0900

    remove: UserCardTitle.tsx

--- components/user-components/AddUserCard.tsx
+++ components/user-components/AddUserCard.tsx
@@ -1,14 +1,14 @@
-import { UserCardTitle } from "./UserCardTitle";
-
 export const AddUserCard = (): JSX.Element => {
   return (
-    <div className="flex items-center border-t border-secondary-regular w-full h-9 md:h-11 lg:h-14 xl:h-[70px] 2xl:h-[84px]">
+    <div className="flex items-center w-full h-9 md:h-11 lg:h-14 xl:h-[70px] 2xl:h-[84px]">
       <button className="border border-secondary-regular rounded-md md:rounded-lg lg:rounded-lg xl:rounded-xl 2xl:rounded-2xl w-6 md:w-[29px] lg:w-[37px] xl:w-[47px] 2xl:w-14 h-2/3 font-bold text-primary-regular text-base md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl !leading-none">
         ＋
       </button>
 
       <div className="ml-1 md:ml-1.5 lg:ml-2 xl:ml-2.5 2xl:ml-3">
-        <UserCardTitle cardType="add-card" title="共同編集者の追加" />
+        <p className="text-primary-regular text-xs lg:text-base xl:text-xl 2xl:text-2xl">
+          共同編集者の追加
+        </p>
       </div>
     </div>
   );
--- components/user-components/UserCard.tsx
+++ components/user-components/UserCard.tsx
@@ -2,7 +2,6 @@ import { useMemo } from "react";
 import Image from "next/image";
 
 import { User } from "../../types";
-import { UserCardTitle } from "./UserCardTitle";
 
 interface Props {
   userData: User;
@@ -23,13 +22,15 @@ export const UserCard = ({ userData }: Props): JSX.Element => {
   }, [userData.role, userData.name]);
 
   return (
-    <div className="flex items-center w-full h-9 md:h-11 lg:h-14 xl:h-[70px] 2xl:h-[84px]">
+    <div className="flex items-center border-t border-secondary-regular w-full h-9 md:h-11 lg:h-14 xl:h-[70px] 2xl:h-[84px]">
       <div className="relative rounded-md md:rounded-lg lg:rounded-lg xl:rounded-xl 2xl:rounded-2xl w-6 md:w-[29px] lg:w-[37px] xl:w-[47px] 2xl:w-14 h-2/3">
         <Image src={iconSrc} layout="fill" />
       </div>
 
       <div className="ml-1 md:ml-1.5 lg:ml-2 xl:ml-2.5 2xl:ml-3">
-        <UserCardTitle cardType="user-card" title={title} />
+        <p className="text-primary-regular text-xs lg:text-base xl:text-xl 2xl:text-2xl font-bold !leading-basic">
+          {title}
+        </p>
 
         <p className="text-[10px] xl:text-xs 2xl:text-sm text-secondary-regular leading-basic">
           @{userId}
