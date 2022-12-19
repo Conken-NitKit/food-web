a3799f1cf96c0a47b518650498574cdb8d24c38f
平田海輝 <umiteru2004@gmail.com>
2022-12-19 15:35:43 +0900

    remove: ユーザー管理ページから不要な変数を削除しました。

--- pages/owner/[storeId]/users.tsx
+++ pages/owner/[storeId]/users.tsx
@@ -5,8 +5,6 @@ import { AddUserCard, UserCard } from "../../../components/user-cards";
 import { User } from "../../../types";
 
 const Users: NextPage = () => {
-  const tips = "システムの共同編集者を管理することができます。";
-
   //TODO: DB実装後にDBのデータに変更すること。現状定数ですが、DB実装後は変数になるのでキャメルケースにしてます。
   const users: User[] = [
     {
@@ -24,7 +22,10 @@ const Users: NextPage = () => {
   ];
 
   return (
-    <ManagementPageLayout type="users" tips={tips}>
+    <ManagementPageLayout
+      type="users"
+      tips="システムの共同編集者を管理することができます。"
+    >
       <div className="mt-2.5 md:mt-3 lg:mt-3.5 xl:mt-5 2xl:mt-6 ml-1.5 md:ml-2 lg:ml-2.5 xl:ml-3 2xl:ml-4 mr-4 md:mr-5 lg:mr-7 xl:mr-9 2xl:mr-11 font-secondary-regular">
         <AddUserCard />
         <ul>
