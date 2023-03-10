fabfbdd2a8a4c89fc9b3d9042f591abca2684942
saezuri510 <saezuru510@gmail.com>
2023-03-10 12:00:07 +0900

    fix: 商品のレイアウトをgridを使わないようにしました。

--- pages/[shopId]/order.tsx
+++ pages/[shopId]/order.tsx
@@ -87,13 +87,13 @@ const Order: NextPage = () => {
             return (
               <li
                 key={content.product.name}
-                className="relative mb-[16px] pt-[18px] pb-[15px] pr-[14px] pl-[11px] w-full h-[94px] bg-navajowhite-a100 rounded-[13px]"
+                className="relative mb-[16px] pt-[18px] pb-[15px] pr-[85px] pl-[11px] w-full h-[94px] bg-navajowhite-a100 rounded-[13px]"
               >
-                <div className="grid grid-cols-5 grid-rows-1">
-                  <div className="flex items-center text-center mt-[5px] w-[64px] h-[48px] font-menu-card font-bold text-[48px]">
+                <div className="flex">
+                  <div className="flex items-center text-center mt-[5px] mr-[6px] w-[64px] h-[48px] font-menu-card font-bold text-[48px]">
                     {content.product.ideogram}
                   </div>
-                  <div className="col-span-3 font-['Mulish'] text-[#121212]">
+                  <div className="w-full font-['Mulish'] text-[#121212]">
                     <div className="mb-[9px] font-bold">
                       {content.product.name}
                     </div>
@@ -101,16 +101,16 @@ const Order: NextPage = () => {
                       {content.product.promotion}
                     </div>
                   </div>
-                </div>
-                <button
-                  type="button"
-                  onClick={addCart}
-                  className="flex items-center justify-center absolute bottom-[0px] right-[0px] w-[78px] h-[34px] bg-red-a100 cursor-pointer rounded-tl-[13px] rounded-br-[13px] font-['Mulish'] text-white-a100 text-[13px] font-extrabold"
-                >
-                  + ADD
-                </button>
-                <div className="absolute bottom-[57px] right-[14px] w-[65px] h-[22px] font-menu-card text-brown-a100 text-[10px] font-bold">
-                  単品 ￥{content.product.price}
+                  <button
+                    type="button"
+                    onClick={addCart}
+                    className="flex items-center justify-center absolute bottom-[0px] right-[0px] w-[78px] h-[34px] bg-red-a100 cursor-pointer rounded-tl-[13px] rounded-br-[13px] font-['Mulish'] text-white-a100 text-[13px] font-extrabold"
+                  >
+                    + ADD
+                  </button>
+                  <div className="absolute bottom-[57px] right-[14px] w-[65px] h-[22px] font-menu-card text-brown-a100 text-[10px] font-bold">
+                    単品 ￥{content.product.price}
+                  </div>
                 </div>
               </li>
             );
