81d7c665bdd79ddb470ad8829128670694cd0620
saezuri510 <saezuru510@gmail.com>
2023-03-11 12:17:05 +0900

    fix: 商品カードはabsoluteではなくflexで指定するようにしました。

--- pages/[shopId]/order.tsx
+++ pages/[shopId]/order.tsx
@@ -74,7 +74,7 @@ const Order: NextPage = () => {
           objectFit="cover"
         />
       </div>
-      <div className="absolute top-[176px] mb-[50px] pt-[32px] pr-[15px] pl-[16px] w-full h-full bg-white-a100 rounded-t-[16px]">
+      <div className="absolute top-[176px] pt-[32px] pr-[15px] pl-[16px] w-full h-full bg-white-a100 rounded-t-[16px]">
         <p className="mb-[16px] text-brown-a100 text-[20px] font-bold">
           コンピュータ研究部
         </p>
@@ -87,31 +87,29 @@ const Order: NextPage = () => {
             return (
               <li
                 key={content.product.name}
-                className="relative mb-[16px] pt-[18px] pb-[15px] pr-[85px] pl-[11px] w-full h-[94px] bg-navajowhite-a100 rounded-[13px]"
+                className="flex relative mb-[16px] pt-[15px] pb-[15px] pr-[14px] pl-[11px] w-full h-[94px] bg-navajowhite-a100 rounded-[13px]"
               >
-                <div className="flex">
-                  <div className="flex items-center text-center mt-[5px] mr-[6px] w-[64px] h-[48px] font-menu-card font-bold text-[48px]">
-                    {content.product.ideogram}
+                <div className="flex items-center text-center mt-[8px] mr-[6px] w-[64px] h-[48px] font-menu-card font-bold text-[48px]">
+                  {content.product.ideogram}
+                </div>
+                <div className="mt-[3px] w-full h-full font-['Mulish'] text-[#121212]">
+                  <div className="mb-[9px] h-[16px] font-bold">
+                    {content.product.name}
                   </div>
-                  <div className="w-full font-['Mulish'] text-[#121212]">
-                    <div className="mb-[9px] font-bold">
-                      {content.product.name}
-                    </div>
-                    <div className="text-[12px] opacity-[0.8] font-light whitespace-pre-line">
-                      {content.product.promotion}
-                    </div>
-                  </div>
-                  <button
-                    type="button"
-                    onClick={addCart}
-                    className="flex items-center justify-center absolute bottom-[0px] right-[0px] w-[78px] h-[34px] bg-red-a100 cursor-pointer rounded-tl-[13px] rounded-br-[13px] font-['Mulish'] text-white-a100 text-[13px] font-extrabold"
-                  >
-                    + ADD
-                  </button>
-                  <div className="absolute bottom-[57px] right-[14px] w-[65px] h-[22px] font-menu-card text-brown-a100 text-[10px] font-bold">
-                    単品 ￥{content.product.price}
+                  <div className="text-[12px] opacity-[0.8] font-light whitespace-pre-line">
+                    {content.product.promotion}
                   </div>
                 </div>
+                <div className="ml-[6px] w-[65px] h-[22px] font-menu-card text-brown-a100 text-[10px] font-bold">
+                  単品 ￥{content.product.price}
+                </div>
+                <button
+                  type="button"
+                  onClick={addCart}
+                  className="flex items-center justify-center absolute bottom-[0px] right-[0px] w-[78px] h-[34px] bg-red-a100 cursor-pointer rounded-tl-[13px] rounded-br-[13px] font-['Mulish'] text-white-a100 text-[13px] font-extrabold"
+                >
+                  + ADD
+                </button>
               </li>
             );
           })}
